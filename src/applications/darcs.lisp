f4930f5afff805204b3085a4dacc25030eee386b
aycan.irican <aycan.irican@core.gen.tr>
2006-12-25 05:46:48 +0200

    improved generation of project source.
    darcs-hash:20061225034648-1ed77-706f1d31cfeb4eb84fe6e8ebc8e1f5800ffd1ef5.gz

--- src/applications/darcs.lisp
+++ src/applications/darcs.lisp
@@ -1,11 +1,71 @@
 (in-package :core-server)
 
-(defmethod serialize ((self darcs-web-appllication))
-  (ensure-directories-exist (darcs-web-appllication.source-pathname self))
-  (when (probe-file (merge-pathnames (make-pathname :directory (list :relative "_darcs"))
-				     (darcs-web-appllication.source-pathname self)))
-    (error "bu proje zaten var."))
-  (mapcar #'(lambda (dir)
-	      (ensure-directories-exist (merge-pathnames (pathname dir))
-					(darcs-web-appllication.source-pathname self)))
-	  (darcs-web-appllication.directory-list self)))
+;; aycan: go functional
+(defun darcs-directory (project-pathname)
+  (merge-pathnames (make-pathname :directory '(:relative "_darcs")) project-pathname))
+
+(defun darcs-author-file (project-pathname)
+  (merge-pathnames (make-pathname :directory '(:relative "prefs") :name "author") (darcs-directory project-pathname)))
+
+(defun darcs-init (project-pathname admin-email)
+  (if (zerop (with-current-directory project-pathname
+	       (sb-ext::process-exit-code
+		(sb-ext::run-program "/usr/bin/darcs" '("init")))))
+      (progn
+	(with-output-to-file (s (darcs-author-file project-pathname))
+	  (format s " <~A>" admin-email)))
+      (error "darcs init failed.")))
+
+;; <project-pathname>/project-name.asd (systems source)
+(defun systems-source (project-pathname project-name)
+  (merge-pathnames (make-pathname :name project-name :type "asd")
+		   project-pathname))
+
+;; <project-pathname>/project-name.lisp (main source)
+(defun main-source (project-pathname project-name)
+  (merge-pathnames (make-pathname :name project-name :type "lisp")
+		   project-pathname))
+
+;; <project-pathname>/src/model.lisp (model source)
+(defun model-source (project-pathname)
+  (merge-pathnames (make-pathname :directory '(:relative "src") :name "model" :type "lisp")
+		   project-pathname))
+
+;; <project-pathname>/src/packages.lisp (packages source)
+(defun packages-source (project-pathname)
+  (merge-pathnames (make-pathname :directory '(:relative "src") :name "packages" :type "lisp")
+		   project-pathname))
+
+(defun touch-file (pathname)
+  (sb-ext::run-program "/usr/bin/touch" (list (format nil "~A" pathname))))
+
+(defmethod serialize ((self darcs-web-application)) 
+  (let ((ppath (make-pathname :directory (darcs-web-application.source-pathname self)))
+	(pname (darcs-web-application.project-name self)))
+    ;; Create project folder
+    (ensure-directories-exist ppath)
+
+    ;; Error if exists
+    (when (probe-file (darcs-directory ppath))
+      (error "Project already has _darcs folder."))
+    
+    ;; Create template folders
+    (mapcar #'(lambda (dir)
+		(ensure-directories-exist (merge-pathnames dir ppath)))
+	    (darcs-web-application.directory-list self))
+
+    ;; Create standard files
+    (with-open-file (s (systems-source ppath pname) :if-does-not-exist :create)
+      )
+
+    (with-open-file (s (main-source ppath pname) :if-does-not-exist :create)
+      )
+
+    (with-open-file (s (model-source ppath) :if-does-not-exist :create)
+      )
+    
+    (with-open-file (s (packages-source ppath) :if-does-not-exist :create)
+      )
+
+    ;; Initiate darcs repo and set author 
+    (darcs-init ppath (web-application.admin-email self))))
