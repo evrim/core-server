57f867704a100261d5d397256c16fca2d0021d60
aycan.irican <aycan.irican@core.gen.tr>
2007-01-24 18:52:37 +0200

    application extends database-server...
    darcs-hash:20070124165237-1ed77-6e85d3f1482aab854ac32e27034e10c12b29ee09.gz

--- etc/darcs/src/project-name.lisp
+++ etc/darcs/src/project-name.lisp
@@ -4,26 +4,24 @@
 (defparameter *wwwroot* #P"project-path/wwwroot/")
 
 (defclass project-name-application (ucw-web-application
+				    database-server
 				    cookie-session-application-module
 				    ajax-application-module)
   ()
-  (:default-initargs 
+  (:default-initargs
    :tal-generator (make-instance 'yaclml:file-system-generator
 				 :cachep t
 				 :root-directories (list *wwwroot* *ucw-tal-root*))
-    :www-roots (list *wwwroot* *ucw-tal-root*)))
+    :www-roots (list *wwwroot* *ucw-tal-root*)
+    :directory *db-location*
+    :db-auto-start t
+    :model-class 'project-name-model))
 
 (defvar *app* (make-instance 'project-name-application
 			     :fqdn project-fqdn
 			     :admin-email project-admin-email
 			     :url-prefix "/project-name/"
 			     :debug-on-error t))
-
-(defvar *db*
-  (make-instance 'database-server
-		 :db-auto-start t
-		 :directory *db-location*
-		 :model-class 'project-name-model))
 
 (defun register-me (server)
   (register-application server *app*))
