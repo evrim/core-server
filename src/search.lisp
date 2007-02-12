338d9a8d5f968fbb5317b8fe64f10d1761d13a12
aycan.irican <aycan.irican@core.gen.tr>
2007-02-12 19:27:28 +0200

    added comments
    darcs-hash:20070212172728-1ed77-89fe31c5fd3a0ff06e3c99a6a79ef2d65688d6d5.gz

--- src/search.lisp
+++ src/search.lisp
@@ -1,5 +1,7 @@
 (in-package :tr.gen.core.server)
 
+;; This is the generic search algorithm which is given in PAIP and
+;; AIMA books.
 (defun core-search (states goal-p successors combiner)
   "Find a state that satisfies the goal-p. Start with states, and
   search according to successors and combiner"
@@ -11,6 +13,7 @@
 		     (rest states))
 	    goal-p successors combiner))))
 
+;; Standard successors
 (defun string-search (str selector)
   #'(lambda (x)
       (string= str (funcall selector x))))
