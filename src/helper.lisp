aa42411a600ce6da09e13a9e8ef7e8a6a07b147e
aycan.irican <aycan.irican@core.gen.tr>
2007-05-10 18:36:09 +0300

    turkish sort and string<
    darcs-hash:20070510153609-1ed77-4648e1f2c720f9b5a8f36c4dc376a5adc1c76fab.gz

--- src/helper.lisp
+++ src/helper.lisp
@@ -72,3 +72,70 @@ string in BIG string."
 
 (defmethod make-keyword ((sym symbol))
   (intern (symbol-name sym) :keyword))
+
+
+(defvar +tr-alphabet+
+  "ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVWXYZabcçdefgğhıijklmnoöpqrsştuüvwxyz")
+
+(defun tr-char-upcase (a)
+  (case a
+    ((#\ı #\i)
+     (aref +tr-alphabet+ (- (position a +tr-alphabet+) 32)))
+    (otherwise (char-upcase a))))
+
+(defun tr-char-downcase (a)
+  (case a
+    ((#\I #\İ)
+     (aref +tr-alphabet+ (+ (position a +tr-alphabet+) 32)))
+    (otherwise (char-downcase a))))
+
+(defun tr-string-upcase (str)
+  (reduce #'(lambda (acc item) (vector-push-extend (tr-char-upcase item) acc) acc)
+	  str
+	  :initial-value (make-array 0 :fill-pointer 0 :element-type 'character)))
+
+(defun tr-string-downcase (str)  
+  (reduce #'(lambda (acc item) (vector-push-extend (tr-char-downcase item) acc) acc)
+	  str
+	  :initial-value (make-array 0 :fill-pointer 0 :element-type 'character)))
+
+(defun tr-char< (a b) 
+  (if (eq a b)
+      nil
+      (let* ((posa (position a +tr-alphabet+))
+	     (posb (position b +tr-alphabet+)))
+	(if (and posa posb)
+	    (> posa posb)
+	    (char< a b)))))
+
+(defun tr-char> (a b)
+  (if (eq a b)
+      nil
+      (not (tr-char< a b))))
+
+;; (and
+;;   (tr-string< "aycan" "AYCAN")
+;;   (tr-string< "aYcaN" "Aycan")
+;;   (tr-string< "aycan" "aycaN")
+;;   (equal (sort (list "ABC" "Abc" "abc" "aBC") #'string<)
+;;          (tr-sort (list "ABC" "Abc" "abc" "aBC") #'tr-string>)))
+
+(defun tr-string< (a b) 
+  (let ((len (min (length a) (length b)))
+  	(result nil))
+    (dotimes (i len)
+      (if (tr-char< (aref a i) (aref b i))
+	  (return-from tr-string< t))
+      (if (tr-char< (aref b i) (aref a i))
+  	  (return-from tr-string< nil)))
+    nil)
+  )
+
+(defun tr-string> (a b)
+  (not (tr-string< a b)))
+
+(defun tr-sort (list test &key (key #'identity)) 
+  (sort (copy-list list)
+	(lambda (a b)
+	  (funcall test a b))
+	:key key))
