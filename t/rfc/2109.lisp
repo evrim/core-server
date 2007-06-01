baa80ff553cd35f6c708e140468516333c6ef7d1
aycan.irican <aycan.irican@core.gen.tr>
2007-06-01 20:41:16 +0300

    added new cookie test
    darcs-hash:20070601174116-1ed77-2d5436ebc471d197ac960307182f09c1074021a2.gz

--- t/rfc/2109.lisp
+++ t/rfc/2109.lisp
@@ -1,15 +1,28 @@
 (in-package :tr.gen.core.server.test)
 
-(deftest test-cookie
+(deftest test-cookie-to-stream
     (let ((cookie-o (make-cookie "neym" "valyu"
 				 :version 2 :comment "coment"
 				 :domain "domeyn" :max-age 0
 				 :path "/acme" :secure t))
 	  (cookie-s "neym=\"valyu\"; Version=\"2\"; Comment=\"coment\"; Domain=\"domeyn\"; Max-age=\"0\"; Path=\"/acme\"; Secure")
 	  (cstream (make-core-stream "")))
-      ;; action
+      ;; write to stream
       (cookie! cstream cookie-o)
-      ;; test 
+      ;; read from stream and compare
       (string= (core-server::stream-data cstream) cookie-s))
   ;; expected result
+  t)
+
+(deftest test-cookie-from-stream
+    (let* ((cstream (make-core-stream "$Version=\"1\";
+Customer=\"WILE_E_COYOTE\"; $Path=\"/acme\"; $Domain=\"gee\"; 
+Part_Number=\"Rocket_Launcher_0001\"; $Path=\"/acme\"; 
+Shipping=\"FedEx\"; $Path=/acme;"))
+	   (cookies (cookie? cstream)))
+      (and (equal 3 (length cookies))
+	   (equal (cookie.name (car cookies)) "Customer")
+	   (equal (cookie.value (car cookies)) "WILE_E_COYOTE")
+	   (equal (cookie.domain (car cookies)) "gee")
+	   (equal (cookie.path (caddr cookies)) "/acme")))
   t)
