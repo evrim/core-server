47d0de1e30ee9a86ea51dfdbbd3184e72b09d032
evrim <evrim@core.gen.tr>
2007-10-13 22:45:41 +0300

    repo make-installer
    darcs-hash:20071013194541-74ae0-93f0d56ec0972f39999031a4ecafbbc2e4b8ab14.gz

---
+++ bin/make_installer.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# [ Core-serveR ] installer 2
+# Episode: Generate from repo
+TAR=`which tar`
+MKTEMP=`which mktemp`
+MKDIR=`which mkdir`
+CP=`which cp`
+DIR=`$MKTEMP -d`
+TARBALL="core-server-installer-`date +\"%Y-%m-%d\"`.tar.gz"
+
+CORESERVER_HOME=`cat src/install/install.sh 2> /dev/null`
+if [ -z $CORESERVER_HOME ]; then
+    CORESERVER_HOME=`cat ../src/install/install.sh 2> /dev/null`;
+    if [ -z $CORESERVER_HOME ]; then
+	echo "Where am i?";
+	exit 1;
+    else
+	CORESERVER_HOME="$(pwd)/../";
+    fi    
+else
+   CORESERVER_HOME="$(pwd)/";
+fi
+echo $CORESERVER_HOME
+$MKDIR -p $DIR/core-server-installer;
+cd $DIR;
+$CP $CORESERVER_HOME/src/install/* core-server-installer;
+$TAR zcf $TARBALL *
+mv $TARBALL /tmp/
+echo "[Core serveR] Installer tarball is ready: /tmp/$TARBALL"
