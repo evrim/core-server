4302d52a6cc6237e03e8b6c7424c17aef89ae103
evrim <evrim@core.gen.tr>
2007-10-17 19:48:36 +0300

    unit.lisp tests.
    darcs-hash:20071017164836-74ae0-653aadaf3aba2c9984b1e58167c00e926a98dc99.gz

--- t/units.lisp
+++ t/units.lisp
@@ -3,34 +3,42 @@
 (defclass test-unit (local-unit)
   ())
 
-(defmethod/unit lightning :async-no-return ((test-unit test-unit) abc def)
-  (list test-unit abc def))
+(defmethod/unit add-numbers ((self test-unit) &rest numbers)
+  (apply #'+ numbers))
 
-(defun fast-lightning (test)
-  (typecase test
-    (core-string-io-stream nil)
-    (core-vector-io-stream nil)
-    (core-fd-io-stream nil)
-    (core-file-io-stream nil)
-    (core-stream nil)
-    (database-server nil)
-    (t nil)))
+(defvar *u (make-instance 'test-unit))
+(assert (= 6 (add-numbers *u 1 2 3)))
+(start *s)
+(assert (= 6 (add-numbers *u 1 2 3)))
 
-(defun speed-of-light (&optional (n 1000))
-  (let ((unit (make-instance 'test-unit)))
-  ;;  (start unit)
+(defun clean-local-units ()
+  (mapcar #'(lambda (thread)
+	      (if (equal "Local Unit" (sb-thread::thread-name thread))
+		  (thread-kill thread)))
+	  (core-server::all-threads)))
+
+;; (defmethod/unit lightning :async-no-return ((test-unit test-unit) abc def)
+;;   (list test-unit abc def))
+
+;; (defun fast-lightning (test)
+;;   (typecase test
+;;     (core-string-io-stream nil)
+;;     (core-vector-io-stream nil)
+;;     (core-fd-io-stream nil)
+;;     (core-file-io-stream nil)
+;;     (core-stream nil)
+;;     (database-server nil)
+;;     (t nil)))
+
+;; (defun speed-of-light (&optional (n 1000))
+;;   (let ((unit (make-instance 'test-unit)))
+;;    (start unit)
 ;;    (unwind-protect (time
 ;; 		    (loop for i from 1 upto n
 ;; 		       do (lightning unit 'gee 33)))
 ;;      (stop unit)
 ;;      )
-     (time (loop for i from 1 upto n
- 	       do (fast-lightning unit)))
-    ))
+;;   ;;    (time (loop for i from 1 upto n
+;; ;;  	       do (fast-lightning unit)))
+;;     ))
 
-
-(defun cleanup ()
-  (mapcar #'(lambda (thread)
-	      (if (equal "Local Unit" (sb-thread::thread-name thread))
-		  (thread-kill thread)))
-	  (core-server::all-threads)))
