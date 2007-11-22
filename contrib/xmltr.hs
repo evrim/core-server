33dba9b95112330c9852fd525eb2fcec653893fa
aycan.irican <aycan.irican@core.gen.tr>
2007-11-22 21:58:14 +0200

    fixed document handling of "xmltr". You should wrap replacement part with a div tag.
    darcs-hash:20071122195814-3f8d4-d583cbcc68c229b683f5a6de262c9671cdb87151.gz

--- contrib/xmltr.hs
+++ contrib/xmltr.hs
@@ -5,7 +5,7 @@ import Text.XML.HXT.Arrow
  
 import System.IO
 import System.Environment
-import System.Console.GetOpt
+-- import System.Console.GetOpt
 import System.Exit
 
 ---
@@ -22,7 +22,7 @@ main
       if rc >= c_err
          then exitWith (ExitFailure (0-1))
          else exitWith ExitSuccess
-                
+
 -- | the dummy for the boring stuff of option evaluation,
 -- usually done with 'System.Console.GetOpt'
  
@@ -31,17 +31,17 @@ cmdlineOpts argv
     = return ([(a_validate, v_0)], argv!!0, argv!!1, argv!!2)
  
 -- | the main arrow
- 
+
 application:: Attributes -> String -> String -> String -> String -> IOSArrow b Int
 application al id src dst text
     = readDocument ([(a_parse_html, v_1)]) src
       >>>
-      processTopDown (addContent id text `when` isElem)
+      processChildren (addContent id text `when` isElem)
       >>>
-      writeDocument [(a_output_html, v_0), (a_indent, v_1), (a_remove_whitespace, v_1), (a_output_encoding, utf8)] dst
+      writeDocument [(a_output_html, v_1), (a_indent, v_1), (a_remove_whitespace, v_1), (a_output_encoding, utf8)] dst
       >>>
       getErrStatus
- 
+
 -- | the dummy for the real processing: the identity filter
 
 addContent:: String -> String -> IOSArrow XmlTree XmlTree 
@@ -57,4 +57,6 @@ addContent id text =
             >>> 
             isA (\x -> x == id)
       insertContent
-          = replaceChildren (readString [(a_encoding, utf8), (a_parse_html, v_1), (a_validate, v_0)] text >>> getChildren)
+          = replaceChildren (readString [(a_parse_html, v_1), (a_encoding, utf8), (a_validate, v_0)] text 
+                             >>> 
+                             getChildren >>> getChildren)
