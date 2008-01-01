03b3c9ee0dbd343902967636fd3d4157294652c3
evrim <evrim@core.gen.tr>
2008-01-01 22:50:02 +0200

    dojo component tests added, not working yet.
    darcs-hash:20080101205002-74ae0-c5a3aed917496449d1b6b912cf03d3f3414ea6f7.gz

--- src/web/components/dojo.lisp
+++ src/web/components/dojo.lisp
@@ -1,13 +1,12 @@
 (in-package :core-server)
 
 (defcomponent dojo-widget (html-element)
-  ((dojo-type :host remote :initarg :dojo-type :initform (error "Please specify :dojo-type"))))
+  ((dojo-type :initarg :dojo-type :initform (error "Please specify :dojo-type"))
+   (dojo-args :initarg :dojo-args :initform '())))
 
 (defmethod/local render ((self dojo-widget))
   (with-yaclml-output-to-string
-    (<:div (<:ul (<:li "Item 1")
-		 (<:li "Item 2")
-		 (<:li "Item 3")))))
+    (<:p "This is a dojo widget.")))
 
 (defmethod/cc send/ctor ((self dojo-widget) remote-slots local-methods remote-methods)
   (<:js
@@ -20,11 +19,12 @@
 	  (if (= "function" (typeof o.render))		
 	      (setf p.inner-h-t-m-l (o.render)))
 	  
-	  (dojo.require "dojo.xml.Parse")
-	  (dojo.require ,(format nil "dojo.widget.~A" (string-capitalize (slot-value self 'dojo-type))))
+;;	  (dojo.require "dojo.xml.Parse")
+	  (dojo.require ,(format nil "~A" (js::symbol-to-js (slot-value self 'dojo-type))))
 ;;; 	  (let ((parser (new (dojo.xml.*parse)))
 ;;; 		(frag (parser.parse-element p nil t)))
 ;;; 	    (.create-components (dojo.widget.get-parser) frag))
-	  (setf this.prototype p)
-	  (return p)))))
+	  (setf this.prototype
+		(new (,(slot-value self 'dojo-type) (create ,@(slot-value self 'dojo-args)) p)))
+	  (return this.prototype)))))
 
