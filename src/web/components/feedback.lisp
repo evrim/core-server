9ff96de0a473fb6ff0d173a92ee08afebda1b831
evrim <evrim@core.gen.tr>
2008-01-23 23:11:28 +0200

    feedback comopnent.
    darcs-hash:20080123211128-74ae0-f32f7786a9f83abe7edfc5ad5a4e2aa2216fff5a.gz

---
+++ src/web/components/feedback.lisp
@@ -0,0 +1,39 @@
+(in-package :core-server)
+
+(defcomponent feedback-component (toaster-component)
+  ((feedback-id :accessor feedback-id :host remote :initform "feedback")
+   (greeting-text :accessor greeting-text :host remote
+		  :initform "Please give us feedback to improve our site.")
+   (thank-text :accessor thank-text :host remote
+	       :initform "Thank you for giving us feedback.")))
+
+(defmethod/remote get-div ((self feedback-component))
+  (return ($ (this.get-feedback-id))))
+
+(defmethod/local feedback-form ((self feedback-component))
+  (<:form
+   (<:input :type "text" :id "feedback-text" :name "feedback-text")))
+
+(defmethod/local send-feedback ((self feedback-component) feedback)
+  (format *standard-output* "got feedback:~A~%" feedback))
+
+(defmethod/remote setup ((self feedback-component))
+  (if (= "undefined" (typeof (this.get-div)))
+      (this.toast "Feedback div not found, aborting feedback component."))
+    
+  (setf (slot-value (this.get-div) 'inner-h-t-m-l) "")
+  (let ((form (this.feedback-form)))
+    (.append-child (this.get-div) form)
+    (let ((input ($ "feedback-text")))      
+      (setf form.onsubmit (dojo.hitch this
+				      (lambda ()
+					(this.toast "Sending...")
+					(this.send-feedback input.value)
+					(this.setup)
+					(this.toast (this.get-thank-text))
+					(return false))))
+      (setf input.value (this.get-greeting-text)
+	    input.onfocus (dojo.hitch this
+				      (lambda ()					
+					(if (= (this.get-greeting-text) input.value)
+					    (setf input.value nil))))))))
