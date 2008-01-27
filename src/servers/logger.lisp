f1d94bdcd025968bcce4389426ff9c7d481aa2e3
aycan.irican <aycan.irican@core.gen.tr>
2008-01-27 20:25:42 +0200

    improved logger-server and mail-service.
    darcs-hash:20080127182542-3f8d4-b393f428df90143649fd12c83e6b56d46933181e.gz

--- src/servers/logger.lisp
+++ src/servers/logger.lisp
@@ -1,14 +1,24 @@
 (in-package :tr.gen.core.server)
 
-(defun default-log-path ()
+;;;;
+;;;; Usage:
+;;;;
+;;
+;; for console output:
+;; (defparameter *logger* (make-instance 'logger-server :log-stream *standard-output*))
+;;
+;; for file output (to $CORESERVER_HOME/var/log/core-server.log):
+;; (defparameter *logger* (make-instance 'logger-server :log-stream nil))
+
+(defparameter +default-log-path+
   (merge-pathnames (make-pathname :directory '(:relative "var" "log"))
 		   (sb-posix:getenv "CORESERVER_HOME")))
 
 ;; this is a thread that logs messages to a stream
 (defclass logger-server (local-unit)
-  ((log-stream :accessor log-stream :initarg :log-stream :initform nil)
+  ((log-stream :accessor log-stream :initarg :log-stream :initform *core-output*)
    (log-path :accessor log-path :initarg :log-path
-	     :initform (default-log-path))))
+	     :initform +default-log-path+)))
 
 ;; log lines are like that:
 ;; <time> <tag> <message>
@@ -19,37 +29,38 @@
   (char! (log-stream self) #\Space)
   (string! (log-stream self) message)
   (char! (log-stream self) #\Newline)
-  (force-output (slot-value (log-stream self) '%stream)))
+  (when (typep (log-stream self) 'core-fd-io-stream)
+    (force-output (slot-value (log-stream self) '%stream))))
 
 ;; raw logging with newline at the end of the message
 (defmethod/unit log-me-raw :async-no-return ((self logger-server) message)
   (string! (log-stream self) message)
-  (char! (log-stream self) #\Newline))
+  (char! (log-stream self) #\Newline)
+  (when (typep (log-stream self) 'core-fd-io-stream)
+    (force-output (slot-value (log-stream self) '%stream))))
 
 ;; Start logger unit
 (defmethod start ((self logger-server))
-  (if (not (eq (log-stream self) *standard-output*))
-      (setf (log-stream self)
-	    (make-core-stream
-	     (open (make-pathname :directory (pathname-directory (log-path self))
-				  :name "core-server"
-				  :type "log")
-		   :direction :output
-		   :element-type '(unsigned-byte 8)
-		   :if-exists :supersede
-		   :if-does-not-exist :create
-		   :external-format :utf8)))))
+  (unless (log-stream self)
+    (setf (log-stream self)
+	  (make-core-stream
+	   (open (make-pathname :directory (pathname-directory (log-path self))
+				:name "core-server"
+				:type "log")
+		 :direction :output
+		 :element-type '(unsigned-byte 8)
+		 :if-exists :supersede
+		 :if-does-not-exist :create
+		 :external-format :utf8)))))
 
 ;; Stop logger unit
 (defmethod stop ((self logger-server))
-  (if (not (eq (log-stream self) *standard-output*))
-      (when (log-stream self)
-	(close-stream (log-stream self))))
-  (setf (log-stream self) nil))
+  (when (and (log-stream self) 
+	     (typep (log-stream self) 'core-fd-io-stream))
+    (close-stream (log-stream self))
+    (setf (log-stream self) nil)))
 
 (defmethod status ((self logger-server))
   (if (log-stream self)
       t
       nil))
-
-;;; (defparameter *logger* (make-instance 'logger-server :log-stream *standard-output*))
--- src/services/mail.lisp
+++ src/services/mail.lisp
@@ -1,13 +1,178 @@
 (in-package :tr.gen.core.server)
 
-;;; You can use whichever server you want. Just create a mail-sender and use it like that:
 
-;;; (defparameter *mail-sender1* (make-instance 'mail-sender
-;;; 					    :username "forexample123@gmail.com"
-;;; 					    :password "secret"
-;;; 					    :server "smtp.gmail.com"
-;;; 					    :port 467))
+;;; TODO: use auth whenever needed.
 
+;;;;
+;;;; Usage:
+;;;;
+;;
+;; (defparameter *s (make-instance 'mail-sender
+;; 				:username "aycan@core.gen.tr"
+;; 				:password "l00kman0h4ndz"
+;; 				:server "mail.core.gen.tr"
+;; 				:port 25))
+;; (start *s)
+;; (mapcar (curry #'enqueue (mail-sender.queue *s)) *test-mails*)
+;; (process *s)
+
+(defparameter *smtp-codes*
+  '((220 . "connect")
+    (235 . "auth success")
+    (250 . "mail from, rcpt to")
+    (334 . "auth plain")
+    (354 . "data")
+    (554 . "fail")))
+
+(defrule default-smtp-response? (c)
+  (:lwsp?)
+  (:fixnum? c)
+  (:zom (:not #\Newline)
+	(:type (or space? octet?)))
+  (:return c))
+
+(defun service-ready? (sock)
+  (eq 220 (default-smtp-response? sock)))
+(defun action-ok? (sock)
+  (eq 250 (default-smtp-response? sock)))
+(defun start-input? (sock)
+  (eq 354 (default-smtp-response? sock)))
+(defun auth-start? (sock)
+  (eq 334 (default-smtp-response? sock)))
+(defun auth-success? (sock)
+  (eq 235 (default-smtp-response? sock)))
+
+(defclass mail-sender (logger-server)
+  ((username :accessor mail-sender.username :initarg :username)
+   (password :accessor mail-sender.password :initarg :password)
+   (server :accessor mail-sender.server :initarg :server)
+   (port :accessor mail-sender.port :initarg :port :initform 25)
+   (queue :accessor mail-sender.queue :initarg :queue :initform (make-instance 'queue))
+   (queue-lock :accessor mail-sender.queue-lock :initarg :queue-lock :initform (sb-thread::make-mutex)))
+  (:default-initargs :name "mail sender"))
+
+(defprint-object (self mail-sender)
+  (format t "server: ~A, port: ~D" (mail-sender.server self) (mail-sender.port self)))
+
+(defclass envelope ()
+  ((recipient :accessor envelope.recipient :initarg :recipient)
+   (subject :accessor envelope.subject :initarg :subject)
+   (text :accessor envelope.text :initarg :text :initform nil)))
+
+(defmethod/unit process-queue ((self mail-sender) socket)
+  (sb-thread::with-recursive-lock ((mail-sender.queue-lock self))
+    (aif (dequeue (mail-sender.queue self))
+	 (progn 
+	   (%sendmail self it socket)
+	   (process-queue self socket))
+	 nil)))
+
+;; write a function that acquires a lock and starts sending mails
+(defmethod/unit process :async-no-return ((self mail-sender))
+  (when (< 0 (queue-count (mail-sender.queue self)))
+    (smsg self (format nil "Connecting ~A:~D." (mail-sender.server self) (mail-sender.port self)))
+    (aif (connect (mail-sender.server self) (mail-sender.port self)) 
+	 (progn
+	   (smsg self "Connected to SMTP Server.")
+	   ;; 220 node2.core.gen.tr ESMTP
+	   (service-ready? it)
+	   (smsg self "Service ready.")
+	   (string! it "HELO core.gen.tr")
+	   (char! it #\Newline)
+	   (finish-output (slot-value it '%stream)) 
+	   ;; 250 node2.core.gen.tr
+	   (action-ok? it)
+	   (smsg self (format nil "Got HELO from ~A." (mail-sender.server self)))
+	   (process-queue self it)
+	   ;; close conn
+	   (close-stream it)
+	   (smsg self "Connection closed."))
+	 (close-stream it))))
+
+(defmethod %sendmail ((self mail-sender) (e envelope) socket)
+  (let ((stream (slot-value socket '%stream)))
+    (flet ((purge ()
+	     (finish-output stream)))
+      ;; TODO: how to program atomically dependent steps in here?
+      (and
+       (progn
+	 (string! socket (format nil "MAIL FROM: ~A" (mail-sender.username self)))
+	 (char! socket #\Newline)
+	 (purge)
+	 ;; 250 2.1.0 Ok
+	 (action-ok? socket))
+       (progn 
+	 (string! socket (format nil "RCPT TO: ~A" (envelope.recipient e)))
+	 (char! socket #\Newline)
+	 (purge)
+	 ;; 250 2.1.5 Ok
+	 (action-ok? socket))
+       (progn
+	 (string! socket (format nil "data"))
+	 (char! socket #\Newline)
+	 (purge)
+	 ;; 354 End data with <CR><LF>.<CR><LF>
+	 (start-input? socket))
+       (progn 
+	 (string! socket (build-message-stream e)) 
+	 (char! socket #\Newline)
+	 (char! socket #\.)
+	 (char! socket #\Newline)
+	 (purge)
+	 ;; 250 2.0.0 Ok: queued as 62E6818ABA
+	 (action-ok? socket))))))
+
+;;; build-message-stream :: Envelope -> String
+(defmethod build-message-stream ((e envelope))
+  (let ((s (make-core-stream "")))
+    (string! s (format nil "Subject: ~A" (envelope.subject e)))
+    (char! s #\Newline)
+    (string! s (format nil "To: ~A" (envelope.recipient e)))
+    (char! s #\Newline)
+    (string! s "MIME-Version: 1.0")
+    (char! s #\Newline)
+    (string! s "Content-Type: text/plain; charset=UTF-8; format=flowed")
+    (char! s #\Newline)
+    (string! s "Content-Transfer-Encoding: 8bit") 
+    (char! s #\Newline)
+    (char! s #\Newline)
+    (string! s (envelope.text e))
+    (char! s #\Newline)
+    (return-stream s)))
+
+(defmethod/unit auth-plain ((self mail-sender))
+  (smsg self "Sending AUTH")
+  (let ((s (s-v '%socket)))
+    (string! s "AUTH PLAIN ")
+    (and
+     (auth-start? s)
+     (base64! s (mail-sender.username self))
+     (auth-start? s)
+     (base64! s (mail-sender.password self)))
+    (char! s #\Newline)
+    (let ((res (parse-line? s)))
+      (smsg self (format nil "got response \"~A\"" res)))))
+
+;; (defmethod/unit log-me ((self mail-sender) tag msg)
+;;   (format t "~A:~A" tag msg))
+
+(defun smsg (msender text)
+  (log-me msender 'smtp (format nil "~A: ~A~%" (unit.name msender) text)))
+
+(defparameter *test-mails*
+  (list
+   (make-instance 'envelope
+		  :recipient "aycan@core.gen.tr"
+		  :subject "first mail (1)"
+		  :text "number one (1)")
+   (make-instance 'envelope
+		  :recipient "aycan@core.gen.tr"
+		  :subject "second mail (2)"
+		  :text "number two (2)")
+   (make-instance 'envelope
+		  :recipient "aycan@core.gen.tr"
+		  :subject "third mail (3)"
+		  :text "number three (3)")))
 
 ;;; sample mail conversation
 ;;;
@@ -25,133 +190,3 @@
 ;;; .
 ;;; 250 2.0.0 Ok: queued as 62E6818ABA
 
-(defparameter *smtp-codes*
-  '((220 . "connect")
-    (235 . "auth success")
-    (250 . "mail from, rcpt to")
-    (334 . "auth plain")
-    (354 . "data")
-    (554 . "fail")))
-
-(defrule default-smtp-response? (c)
-  (:lwsp?)
-  (:fixnum? c)
-  (:zom (:not #\Newline)
-	(:type (or space? visible-char?)))
-  (:return c))
-
-(defun service-ready? (sock)
-  (eq 220 (default-smtp-response? sock)))
-(defun action-ok? (sock)
-  (eq 250 (default-smtp-response? sock)))
-(defun start-input? (sock)
-  (eq 354 (default-smtp-response? sock)))
-(defun auth-start? (sock)
-  (eq 334 (default-smtp-response? sock)))
-(defun auth-success? (sock)
-  (eq 235 (default-smtp-response? sock)))
-
-(defclass mail-sender (local-unit)
-  ((name :accessor mail-sender.name :initarg :name :initform "Mail Sender 1")
-   (%socket :initform nil)
-   (username :accessor mail-sender.username :initarg :username)
-   (password :accessor mail-sender.password :initarg :password)
-   (server :accessor mail-sender.server :initarg :server)
-   (port :accessor mail-sender.port :initarg :port :initform 25)))
-
-(defprint-object (self mail-sender)
-  (format t "server: ~A, port: ~D" (mail-sender.server self) (mail-sender.port self)))
-
-(defmethod/unit sendmail :async-no-return ((self mail-sender) rcpt message)
-  (let* ((sock (s-v '%socket)))
-    ;; TODO: how to program atomically dependent steps in here?
-    (and     
-     (prog1
-	 (action-ok? sock) 
-       (string! sock (format nil "MAIL FROM: ~A" (mail-sender.username self)))
-       (char! sock #\Newline)
-       (finish-output (slot-value sock '%stream)))
-     (prog1
-	 (action-ok? sock) 
-       (string! sock (format nil "RCPT TO: ~A" rcpt))
-       (char! sock #\Newline)
-       (finish-output (slot-value sock '%stream)))
-     (prog1
-	 (action-ok? sock) 
-       (string! sock (format nil "data"))
-       (char! sock #\Newline)
-       (finish-output (slot-value sock '%stream)))
-     (prog1
-	 (start-input? sock) 
-       (string! sock message) 
-       (char! sock #\Newline)
-       (char! sock #\.)
-       (char! sock #\Newline)
-       (finish-output (slot-value sock '%stream)))
-     (action-ok? sock))))
-
-
-;;; make-mail :: String -> String -> String
-(defun make-mail (subject message)
-  (let ((s (make-core-stream "")))
-    (string! s (format nil "Subject: ~A" (tr2en subject)))
-    (char! s #\Newline)
-    (string! s "MIME-Version: 1.0")
-    (char! s #\Newline)
-    (string! s "Content-Type: text/plain; charset=UTF-8; format=flowed")
-    (char! s #\Newline)
-    (string! s "Content-Transfer-Encoding: 8bit") 
-    (char! s #\Newline)
-    (char! s #\Newline)
-    (string! s message)
-    (char! s #\Newline)
-    (return-stream s)))
-
-(defmethod/unit auth-plain ((self mail-sender))
-  (smsg self "Sending AUTH")
-  (let ((s (s-v '%socket)))
-    (string! s "AUTH PLAIN ")
-    (and
-     (auth-start? s)
-     (base64! s (mail-sender.username self))
-     (auth-start? s)
-     (base64! s (mail-sender.password self))
-     )
-    
-    (char! s #\Newline)
-    (let ((res (parse-line? s)))
-      (smsg self (format nil "got response \"~A\"" res)))))
-
-(defun smsg (msender text)
-  (log-me *logger* 'smtp (format nil "~A: ~A" (mail-sender.name msender) text)))
-
-(defmethod start ((self mail-sender))
-  (smsg self "Starting.")
-  (aif (connect (mail-sender.server self) (mail-sender.port self)) 
-       (progn
-	 (smsg self "Connected to SMTP Server.")
-	 (setf (s-v '%socket) it)
-	 ;; connect
-	 (service-ready? it)
-	 (smsg self "Service ready.")
-	 ;; hello
-	 (string! it "HELO core.gen.tr")
-	 (char! it #\Newline)
-	 (finish-output (slot-value (s-v '%socket) '%stream))
-	 ;; get greets
-	 (action-ok? it)
-	 (smsg self "Got helo."))
-       (close-stream it)))
-
-(defmethod stop ((self mail-sender))
-  (when (s-v '%socket)
-    (close-stream (s-v '%socket))
-    (setf (s-v '%socket) nil)
-    (smsg self "Connection closed.")))
-
-(defmethod status ((self mail-sender))
-  (slot-value self '%socket))
-
-;;; Usage:
-;;; (defparameter *s (make-instance 'mail-sender :from "aycan@core.gen.tr" :server "mail.core.gen.tr" :port 25))
-;;; (send-message *s "evrim@core.gen.tr" "y00 mama check diz out")
