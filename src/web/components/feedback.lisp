be2ab04a6f22c374c052af56521e66d50c561ab9
aycan.irican <aycan.irican@core.gen.tr>
2008-02-07 22:26:10 +0200

    fixed feedback component, reflecting sendmail api change
    darcs-hash:20080207202610-3f8d4-4755c1358027888a6d8337508cbd7dd53fbf74db.gz

--- src/web/components/feedback.lisp
+++ src/web/components/feedback.lisp
@@ -5,7 +5,8 @@
    (greeting-text :accessor greeting-text :host remote
 		  :initform "Please give us feedback to improve our site. Click here to enter.")
    (thank-text :accessor thank-text :host remote
-	       :initform "Thank you for giving us feedback.")))
+	       :initform "Thank you for giving us feedback.")
+   (feedback-from :accessor feedback-from :host local :initform "nospam@core.gen.tr")))
 
 (defmethod/remote get-div ((self feedback-component))
   (return ($ (this.get-feedback-id))))
@@ -15,11 +16,27 @@
    (<:input :type "text" :id "feedback-text" :name "feedback-text")))
 
 (defmethod/local send-feedback ((self feedback-component) feedback url)
-  (sendmail (application.server (application self)) (web-application.admin-email (application self)) "Feedback"
-	    (format nil "Date:~A~%Url:~A~%Feedback:~A~%"
-		    (time->string (get-universal-time) :long)
-		    url
-		    feedback)))
+  (sendmail (application.server (application self))
+	    (feedback-from self)
+	    (web-application.admin-email (application self))
+	    "Feedback"
+	    (with-core-stream (s "")
+	      (with-html-output s
+		(<:html
+		 (<:head (<:title "Feedback"))
+		 (<:body
+		  (<:p (format nil "We have got a feedback for ~A." (web-application.fqdn (application self))))
+		  (<:table
+		   (<:tr
+		    (<:td "Date:")
+		    (<:td (time->string (get-universal-time) :long)))
+		   (<:tr
+		    (<:td "Url:")
+		    (<:td url))
+		   (<:tr
+		    (<:td "Text:")
+		    (<:td feedback))))))
+	      (return-stream s))))
 
 (defmethod/remote setup ((self feedback-component))
   (if (= "undefined" (typeof (this.get-div)))
