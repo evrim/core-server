29e86136815fdd2add7caf596cdd5396a51e3887
aycan.irican <aycan.irican@core.gen.tr>
2008-06-07 16:40:39 +0300

    removed unused files core.lisp and ucw.lisp
    darcs-hash:20080607134039-3f8d4-30cad923ae2971c549fc9f96e293cd0a3db5fd0f.gz

--- src/servers/core.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :tr.gen.core.server)
-
-;; (defclass core-server (apache-server name-server ucw-server postfix-server)
-;;   ())
-
-;; (defclass core-web-server (apache-server ucw-server)
-;;   ())
