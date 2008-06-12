7bf42baea800650bf2a56150f1c5fd30eeee165e
evrim <evrim@core.gen.tr>
2008-06-12 03:51:45 +0300

    return to HTML, forget about XHTML
    darcs-hash:20080612005145-74ae0-5889216fa54ae0d0dc62df8167f74cce8b918d6c.gz

--- src/html.lisp
+++ src/html.lisp
@@ -42,10 +42,10 @@
     (format stream "tag:~A" (tag self))))
 
 (defun make-dom-element (tag attributes &rest children)
-  (make-instance 'dom-element
-		 :tag tag
-		 :attributes attributes
-		 :children children))
+  (make-instance 'dom-element :tag tag :attributes attributes :children children))
+
+(defun dom-successor (element)
+  (if (typep element 'dom-element) (children element)))
 
 (defrule attribute-name? (c (acc (make-accumulator)))
   (:oom (:or (:type alphanum? c)
@@ -94,9 +94,22 @@
   (:if (> (length acc) 0)
        (:return acc)))
 
-(defrule dom-element? (tag attr attrs child children)
-  (:checkpoint (:seq "<!DOCTYPE")
+(defrule html-comment? (c (acc (make-accumulator)))
+  (:seq "<!--")
+  (:collect #\< acc) (:collect #\! acc)
+  (:collect #\- acc) (:collect #\- acc)
+  (:zom (:not (:seq "-->"))
+	(:type octet? c)
+	(:collect c acc))
+  (:collect #\- acc) (:collect #\- acc) (:collect #\> acc)
+  (:return acc))
+
+(defparser dom-element? (tag attr attrs child children)
+  (:lwsp?)
+  (:checkpoint (:seq "<?xml")
 	       (:zom (:not #\>) (:type octet?)) (:lwsp?) (:commit))
+  (:checkpoint (:seq "<!DOCTYPE")
+	       (:zom (:not #\>) (:type octet?)) (:lwsp?) (:commit))  
   #\<
   (:tag-name? tag)
   (:zom (:lwsp?)
@@ -105,25 +118,20 @@
   (:or (:and (:lwsp?) (:seq "/>") (:return (make-dom-element tag (nreverse attrs))))
        (:and #\>
 	     (:zom (:lwsp?)
-		   (:or (:dom-element? child) (:text-node? child))
+		   (:or (:dom-element? child) (:html-comment? child) (:text-node? child))
 		   (:do (push child children)))
 	     (:seq "</")
 	     (:seq tag)
 	     #\>
 	     (:return (apply #'make-dom-element tag (nreverse attrs) (nreverse children))))))
 
-(defparameter *dom-parsers* '(dom-element? tag-name? attribute? attribute-value? attribute-name? text-node?
-			      make-dom-element dom-element!))
+(defparameter *dom-parsers*
+  '(dom-element? tag-name? attribute? attribute-value? attribute-name? text-node?
+    html-comment? make-dom-element dom-element!))
 
-(defun trace-dom-parsers ()
-  (mapcar (lambda (fun)
-	    (eval `(trace ,fun)))
-	  *dom-parsers*))
+(defun trace-dom-parsers () (mapcar (lambda (fun) (eval `(trace ,fun))) *dom-parsers*))
 
-(defun untrace-dom-parsers ()
-  (mapcar (lambda (fun)
-	    (eval `(untrace ,fun)))
-	  *dom-parsers*))
+(defun untrace-dom-parsers () (mapcar (lambda (fun) (eval `(untrace ,fun))) *dom-parsers*))
 
 (defparameter +indentation-increment+ 1)
 
@@ -158,33 +166,42 @@
 		(char! stream #\")
 		(string! stream (cdr attr))
 		(char! stream #\"))
-	      (attributes element))
+	      (attributes element))      
+      (char! stream #\>)
       (cond
-	((null (children element))
-	 (string! stream "/>"))
-	(t
-	 (char! stream #\>)
+	((eq 0 (length (children element)))
+	 )
+	((eq 1 (length (children element)))
+	 (mapcar #'child! (children element)))
+	(t	 
 	 (mapcar #'child! (children element))
 	 (char! stream #\Newline)
-	 (indent)
-	 (string! stream "</")
-	 (string! stream (tag element))
-	 (char! stream #\>))))))
+	 (indent)))
+      (string! stream "</")
+      (string! stream (tag element))
+      (char! stream #\>))))
 
-(defmethod dom2js ((element dom-element))  
-  `((lambda ()
-      (let ((elem (document.create-element ,(tag element))))
-	,@(mapcar (lambda (attr)
-		    (if (equal (car attr) "class")
-			`(setf (slot-value elem 'class-name) ,(cdr attr))
-			`(setf (slot-value elem ,(car attr)) ,(cdr attr))))
-		  (attributes element))
-	,@(mapcar (lambda (child)
-		    (if (stringp child)
-			`(elem.append-child (document.create-text-node ,child))
-			`(elem.append-child ,(dom2js child))))
-		  (children element))
-	(return elem)))))
+(defclass empty-dom-element (dom-element)
+  ())
+
+(defmethod dom-element! ((stream core-stream) (element empty-dom-element) &optional (indentation 0))
+  (flet ((indent ()
+	   (dotimes (i indentation)
+	     (char! stream #\Space)
+	     (char! stream #\Space))))
+    (prog1 stream
+      (indent)
+      (char! stream #\<)
+      (string! stream (tag element))
+      (mapcar (lambda (attr)
+		(char! stream #\Space)
+		(string! stream (car attr))
+		(char! stream #\=)
+		(char! stream #\")
+		(string! stream (cdr attr))
+		(char! stream #\"))
+	      (attributes element))
+      (string! stream "/>"))))
 
 ;; TODO: Implement validator
 (defmethod validate-dom-tree ((dom-element t))
@@ -196,27 +213,6 @@
        (change-class dom-element it))
   (setf (children dom-element) (mapcar #'validate-dom-tree (children dom-element)))
   dom-element)
-
-(defmacro <:ai (&body body)
-  `(list ,@body))
-
-(defmacro <:ah (&body body)
-  `(list ,@body))
-
-(defmacro <:js (&body body)
-  (with-unique-names (output)
-    `(let ((,output (if +context+ (http-response.stream (response +context+)) *core-output*)))
-       (prog1 (with-html-output ,output
-		(js:js* ,@body))
-	 (char! ,output #\Newline)))))
-
-(defmacro with-html-output (stream &body body)
-  (with-unique-names (element)
-    `(progn
-       ,@(mapcar #'(lambda (elem)
-		     `(let ((,element ,elem))
-			(dom-element! ,stream ,element)))
-		 body))))
 
 (eval-when (:compile-toplevel :execute :load-toplevel)
   (defvar +html-attributes+
@@ -244,7 +240,7 @@
      (values (nreverse acc) arguments))))
 
 (defmacro deftag (name &rest attributes)
-  (let ((attributes (make-attributes attributes)))    
+  (let ((attributes (make-attributes attributes)))
     `(prog1 (defclass ,name (dom-element)
 	      ())
        (defun ,name (&rest args)
@@ -261,6 +257,24 @@
 		     children)))))
        (export ',name (find-package :tr.gen.core.server.html)))))
 
+(defmacro defempty-tag (name &rest attributes)
+  (let ((attributes (make-attributes attributes)))    
+    `(prog1 (defclass ,name (empty-dom-element)
+	      ())
+       (defun ,name (&rest args)
+	 (multiple-value-bind (attributes) (tag-attributes args)
+	   (destructuring-bind (&key ,@attributes) attributes
+	     (validate-dom-tree
+	      (apply #'make-dom-element ,(symbol-name name)		     
+		     (remove-if (lambda (attr)
+				  (if (null (cdr attr))
+				      t))
+				(list ,@(mapcar (lambda (attr)
+						  `(cons ,(symbol-to-js attr) ,attr))
+						attributes)))
+		     nil)))))
+       (export ',name (find-package :tr.gen.core.server.html)))))
+
 ;; HTmL4 Tag Definitions
 ;; http://www.w3.org/TR/xhtml1/dtds.html
 (deftag <:a :core :i18n :event accesskey charset coords href hreflang
@@ -268,34 +282,34 @@
 (deftag <:abbr :core :event :i18n)
 (deftag <:acronym :core :event :i18n)
 (deftag <:address :core :event :i18n)
-(deftag <:area :core :event :i18n alt accesskey coords href nohref
-	onblur onfocus shape tabindex)
+(defempty-tag <:area :core :event :i18n alt accesskey coords href nohref
+	      onblur onfocus shape tabindex)
 (deftag <:b :core :event :i18n)
-(deftag <:base href)
+(defempty-tag <:base href)
 (deftag <:bdo :i18n id style title)
 (deftag <:big :core :event :i18n)
 (deftag <:blockquote :core :event :i18n cite)
 (deftag <:body :core :i18n :event onload onunload)
-(deftag <:br :core)
+(defempty-tag <:br :core)
 (deftag <:button :core :event :i18n accesskey disabled name onblur
 	onfocus tabindex type value)
 (deftag <:caption :core :event :i18n)
 (deftag <:cite :core :event :i18n)
 (deftag <:code :core :event :i18n)
-(deftag <:col :core :event :i18n align char charoff span valign width)
+(defempty-tag <:col :core :event :i18n align char charoff span valign width)
 (deftag <:colgroup :core :event :i18n align char charoff span valign width)
 (deftag <:dd :core :event :i18n)
 (deftag <:del :core :event :i18n cite datetime)
 (deftag <:dfn :core :event :i18n)
-(deftag <:div :core :event :i18n)
+(deftag <:div :core :event :i18n dojo-type open)
 (deftag <:dl :core :event :i18n)
 (deftag <:dt :core :event :i18n)
 (deftag <:em :core :event :i18n)
 (deftag <:fieldset :core :event :i18n)
 (deftag <:form :core :event :i18n action accept-charset enctype method
               name onreset onsubmit target)
-(deftag <:frame :core frameborder longdesc marginheight marginwidth
-                    noresize scrolling src)
+(defempty-tag <:frame :core frameborder longdesc marginheight marginwidth
+	      noresize scrolling src)
 (deftag <:frameset :core cols onload olunload rows)
 (deftag <:h1 :core :event :i18n)
 (deftag <:h2 :core :event :i18n)
@@ -304,21 +318,26 @@
 (deftag <:h5 :core :event :i18n)
 (deftag <:h6 :core :event :i18n)
 (deftag <:head :i18n profile)
-(deftag <:hr :core :event width align)
+(defempty-tag <:hr :core :event width align)
 
 (deftag <:html dir lang)
 (defmethod dom-element! ((stream core-stream) (element <:html) &optional (indentation 0))
-  (string! stream "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">")
+;;  (string! stream "<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+  (char! stream #\Newline)  
+;;;   (string! stream "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
+;;;     \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">")
+  (string! stream "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
+     \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">")   
   (char! stream #\Newline)
   (call-next-method stream element indentation))
 
 (deftag <:i :core :event :i18n)
 (deftag <:iframe :core frameborder longdesc marginheight marginwidth name scrolling src)
-(deftag <:img :core :event :i18n alt src height ismap longdesc usemap width)
-(deftag <:input :core :event :i18n accept accesskey alt checked
-                    disabled maxlength name onblur onchange onfocus
-                    onselect readonly size src tabindex type usemap
-                    value width height)
+(defempty-tag <:img :core :event :i18n alt src height ismap longdesc usemap width)
+(defempty-tag <:input :core :event :i18n accept accesskey alt checked
+	      disabled maxlength name onblur onchange onfocus
+	      onselect readonly size src tabindex type usemap
+	      value width height)
 (deftag <:ins :core :event :i18n cite datetime)
 (deftag <:kbd :core :event :i18n)
 (deftag <:label :core :event :i18n accesskey for onblur onfocus)
@@ -336,7 +355,7 @@
 (deftag <:optgroup :core :event :i18n label disabled)
 (deftag <:option :core :event :i18n disabled label selected value)
 (deftag <:p :core :event :i18n)
-(deftag <:param name id type value valuetype)
+(defempty-tag <:param name id type value valuetype)
 (deftag <:pre :core :event :i18n)
 (deftag <:q :core :event :i18n cite)
 (deftag <:samp :core :event :i18n)
@@ -367,6 +386,71 @@
 (deftag <:var :core :event :i18n)
 (deftag <:embed src width height type quality bgcolor name align
               allow-script-access pluginspage)
+
+(defmethod dom2js ((element t))
+  nil)
+
+(defmethod dom2js ((element dom-element))  
+  `((lambda ()
+      (let ((elem (document.create-element ,(tag element))))
+	,@(mapcar (lambda (attr)
+		    (if (equal (car attr) "class")			 
+			`(setf (slot-value elem 'class-name) ,(cdr attr))
+			`(setf (slot-value elem ,(car attr)) ,(cdr attr))))
+		  (attributes element))
+	,@(mapcar (lambda (child)
+		    (if (stringp child)
+			`(elem.append-child (document.create-text-node ,child))
+			`(elem.append-child ,(dom2js child))))
+		  (children element))
+	(return elem)))))
+
+
+(defmacro <:ai (&body body)
+  `(list ,@body))
+
+(defmacro <:ah (&body body)
+  `(list ,@body))
+
+(defmacro <:js (&body body)
+  (with-unique-names (output)
+    `(let ((,output (if +context+ (http-response.stream (response +context+)) *core-output*)))
+       (prog1 (with-html-output ,output
+		(js:js* ,@body))
+	 (char! ,output #\Newline)))))
+
+(defmacro with-html-output (stream &body body)
+  (with-unique-names (element)
+    `(progn
+       ,@(mapcar #'(lambda (elem)
+		     `(let ((,element ,elem))
+			(dom-element! ,stream ,element)))
+		 body))))
+
+
+;; (defclass dojo-element (dom-element)
+;;   ())
+
+;; (defmacro defdojo (name &rest attributes)
+;;   (let ((attributes (make-attributes attributes))
+;; 	(symbol-package (symbol-package name)))
+;;     `(prog1 (defclass ,name (dojo-element)
+;; 	      ())
+;;        (defun ,name (&rest args)
+;; 	 (multiple-value-bind (attributes children) (tag-attributes args)
+;; 	   (destructuring-bind (&key ,@attributes) attributes
+;; 	     (validate-dom-tree
+;; 	      (apply #'make-dom-element ,(symbol-name name)		     
+;; 		     (remove-if (lambda (attr)
+;; 				  (if (null (cdr attr))
+;; 				      t))
+;; 				(list ,@(mapcar (lambda (attr)
+;; 						  `(cons ,(symbol-to-js attr) ,attr))
+;; 						attributes)))
+;; 		     children)))))
+;;        (export ',name (find-package ,(package-name symbol-package))))))
+
+;; (defdojo <dijit::dialog open duration title)
 
 ;; (defvar *current-selected* nil)
 ;; (defun <:css (selectors &allow-other-attributes others &body body)
