c29897e658d6ac29967a27b8e11d34fa6e62a035
evrim <evrim@core.gen.tr>
2008-07-07 03:35:16 +0300

    ast-search, ast-search-type added to src/util/search, we can search lisp ast's.
    darcs-hash:20080707003516-74ae0-1e646e4b713d86c9fd4b59487de899bb64ebe953.gz

--- src/util/search.lisp
+++ src/util/search.lisp
@@ -44,3 +44,26 @@
   "Returns a lambda that would match the integer 'int'"
   #'(lambda (x)
       (eq int (funcall selector x))))
+
+;;-----------------------------------------------------------------------------
+;; AST Search Utilities
+;;-----------------------------------------------------------------------------
+;;
+;; These utilities are used to search arnesi lisp2 forms.
+;; see core-server/lib/arnesi/src/walk.lisp
+;;
+(defun ast-search (form goal-p &optional (successor #'form-successor)
+		                         (combiner #'append))
+  (core-search (list form) goal-p successor combiner))
+
+(defun ast-search-type (form types)
+  (let (lst)
+    (ast-search form
+		(lambda (f)
+		  (if (atom types)
+		      (if (typep f types)
+			  (push f lst))
+		      (if (any (lambda (a) (typep f a)) types)
+			  (push f lst)))		  
+		  nil))
+    lst))
