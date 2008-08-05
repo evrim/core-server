9696b15a5e98ac3e7b331331029ec38ec6a71eb3
aycan.irican <aycan.irican@core.gen.tr>
2008-08-05 21:39:05 +0300

    removed debianfix.sh, no longer needed
    darcs-hash:20080805183905-3f8d4-2293a029fb9921b01b2cf2fffc0f6f9271dd32e9.gz

--- src/install/debianfix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-PWD=`pwd`
-cd $1/lib/rfc2388 && yes | darcs unpull --from-match 'date "Sat Jun 16 15:25:52 EEST 2007"'
-cd $1/lib/ucw_dev && yes | darcs unpull --from-match 'date "Thu Jul 12 06:15:54 EEST 2007"'
-cd $PWD
-
