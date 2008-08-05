3c4fe38bda055c208597649a63629df0337ff4ef
aycan.irican <aycan.irican@core.gen.tr>
2008-08-05 21:58:09 +0300

    cosmetics for hello.lisp
    darcs-hash:20080805185809-3f8d4-ddb4d59a204eda0532b454b1055f69d8cc44fdfd.gz

--- examples/hello.lisp
+++ examples/hello.lisp
@@ -1,37 +1,31 @@
-;; a new namespace
+;; Hello World Web Example
+;;
+;; load the file with C-c C-l and visit, 
+;; http://localhost:8080/hello
+
+;; defining a new namespace
 (defpackage :hello
   (:use :cl :core-server :arnesi))
 
-;; switch to new namespace
+;; Switch to new namespace
 (in-package :hello)
 
-;; create application object
+;; Create application object
 (defparameter *hello*
   (make-instance 'http-application
 		 :fqdn "localhost"
 		 :admin-email "aycan@core.gen.tr"))
 
-;; create a handler
-(defurl *hello* "hello" ()
-  (template
-   (<:p "Hello, World!")))
-
-(defun/cc template (body)
+;; Our page is a function which gets body as a parameter
+(defun/cc page (body)
   (<:html
    (<:body
     body)))
 
-;; register application
+;; Create a handler
+(defurl *hello* "hello" ()
+  (page
+   (<:p "Hello, World!")))
+
+;; Register application
 (register *server* *hello*)
-
-;; load the file with C-c C-l and visit http://localhost:8080/hello
-
-;; TODO:
-;;
-;; - We need to simplify importing other packages, why we need arnesi?
-;; - or js?
-;;
-;; - we need to simplify html-output and introduce more content types
-;;   like text/plain etc..
-;;
-;; - 
