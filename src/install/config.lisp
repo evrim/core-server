4612b9468fc2aa11b078a41a021a88f5f4027ec9
aycan.irican <aycan.irican@core.gen.tr>
2008-08-18 03:25:14 +0300

    removed configuration phase from the installer
    darcs-hash:20080818002514-3f8d4-159866b694775296da3d11c5260d380065dc5c65.gz

--- src/install/config.lisp
+++ src/install/config.lisp
@@ -1,11 +1,7 @@
 (in-package :core-server)
 
-(defun config-1.0 (layout)  
-  (link-systems layout))
-
 (defvar +target-directory+ "/opt/core-server/")
 (defparameter *layout* (if (zerop (sb-posix:geteuid))
 			   (make-server-layout +target-directory+)
 			   (make-layout +target-directory+)))
 (install *layout*)
-(config-1.0 *layout*)
