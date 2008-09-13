af44a44980d8353c03af3c607aa2992fa901a45f
evrim <evrim@core.gen.tr>
2008-09-14 02:33:26 +0300

    migrated prevalence server to cl-prevlaence package.
    darcs-hash:20080913233326-74ae0-ac1f6b15fed8c1dc3010fd615ec662959e2ae5b2.gz

--- src/servers/database.lisp
+++ src/servers/database.lisp
@@ -23,119 +23,46 @@
 ;;
 ;; This file contains base in-memory database server.
 ;;
-(defun start-prevalence-system (system)
-  "Restores and returns 'system'"
-  (let ((directory (get-directory system)))    
-    (ensure-directories-exist directory)
-    (setf (cl-prevalence::get-snapshot system)
-	  (merge-pathnames (make-pathname :name (cl-prevalence::get-snapshot-filename system) 
-					  :type (cl-prevalence::get-file-extension system)) 
-			   directory))
-    (setf (cl-prevalence::get-transaction-log system) 
-	  (merge-pathnames (make-pathname :name (cl-prevalence::get-transaction-log-filename system)
-					  :type (cl-prevalence::get-file-extension system))
-			   directory)))
-  (restore system))
-
-(defun open-transaction-log-stream (system)
-  "Opens transaction log streams"
-  (with-slots (cl-prevalence::transaction-log-stream) system
-    (unless cl-prevalence::transaction-log-stream
-      (setf cl-prevalence::transaction-log-stream (open (cl-prevalence::get-transaction-log system)
-					 :direction :output :if-does-not-exist :create :if-exists :append)))))
-
-(defmethod cl-prevalence::initialize-instance :after
-    ((system prevalence-system) &rest initargs &key &allow-other-keys)
-  "After a system is initialized, derive its file paths and try to restore it"
-  (declare (ignore initargs))
-  (unless (typep system 'database-server)
-    (start-prevalence-system system)))
-
-(defun create-guard-with-mutex (mutex)
-  "Returns a lambda that would execute 'thunk' while holding lock 'mutext'"
-  #'(lambda (thunk)
-      (sb-thread::with-mutex (mutex)
-	(funcall thunk))))
-
-(defmethod model ((system prevalence-system))
-  "Returns :model from 'system's root, can be nil"
-  (get-root-object system :model))
-
-(defun tx-model-create (system model-class)
-  "Transaction function that is used to create an instance of
-'model-class'. This instance is then set to :model keyword inside
-'system's root"
-  (setf (get-root-object system :model) (make-instance model-class)))
-
-(defun tx-set-creation-date (system date)
-  "Transaction function that is used to set creation date of the
-model."
-  (setf (standard-model-class.creation-date (get-root-object system :model)) date))
+(deftransaction set-creation-date ((system database-server) date)
+  "Transaction function that is used to set creation date of the model."
+  (setf (get-root-object system :creation-date) date))
 
 (defmethod snapshot :after ((self database-server))
-  (open-transaction-log-stream self))
+  (cl-prevalence::open-transaction-log-stream self))
 
-(defmethod shared-initialize :after ((self database-server) slot-name
-				     &rest initargs &key &allow-other-keys)
-  "Start database if auto-start it t"
-  (declare (ignorable initargs))
-  (when (database-server.db-auto-start self)
-    (%start-database self)))
+(defmethod initialize-instance :after ((system prevalence-system) &rest initargs &key &allow-other-keys)
+  "After a system is initialized, derive its file paths and try to restore it"
+  (declare (ignore initargs))
+  (when (not (typep system 'database-server))
+    (format t "restroing")
+    (restore system)))
 
-(defmethod %start-database ((self database-server))
-  (start-prevalence-system self)
-  (open-transaction-log-stream self)
-  ;; synchronize transactions with a mutex
-  (setf (get-guard self) 
-	(create-guard-with-mutex (sb-thread::make-mutex :name "tx-mutex")))
-  (unless (get-root-object self :id-counter)
-    ;; create global id
-    (execute self (make-transaction 'tx-create-id-counter)))
-  ;; create model
-  (unless (get-root-object self :model)
-    ;; create a model
-    (awhen (database-server.model-class self)
-      (execute self (make-transaction 'tx-model-create it)))
-    (when (typep (get-root-object self :model) 'standard-model-class)
-      (execute self (make-transaction 'tx-set-creation-date (get-universal-time))))))
-
-(defmethod start ((self database-server))
-  "Starts database server"
-  (%start-database self))
-
-(defmethod stop ((self database-server))
-  "Stops database server"
-  (cl-prevalence::close-open-streams self)
-  (setf (cl-prevalence::get-root-objects self) (make-hash-table :test 'eq))
-  (setf (slot-value self 'cl-prevalence::serialization-state) (cl-prevalence::make-serialization-state))
-  (setf (cl-prevalence::get-snapshot self) nil)
-  (setf (cl-prevalence::get-transaction-log self) nil))
-
-;;; This is here since i don't want the database to be
-;;; corrupted. 
 (defmethod database-server.status ((self database-server))
-  (and (slot-value self 'cl-prevalence::transaction-log-stream) t))
+  (and (slot-value self 'cl-prevalence::prevalence.transaction-log-stream) t))
 
 (defmethod status ((self database-server))
   (database-server.status self))
 
-(defun make-database (db-location model-class)
-  "Create a new database at 'db-location' with an instance of 'model-class'"
-  (let ((system (make-prevalence-system db-location
-					:prevalence-system-class 'guarded-prevalence-system
-					:init-args '(:serializer cl-prevalence::serialize-sexp
-						     :deserializer cl-prevalence::deserialize-sexp
-						     :file-extension "sexp"))))
-    ;; synchronize transactions with a mutex
-    (setf (get-guard system) (create-guard-with-mutex (sb-thread::make-mutex :name "database-mutex")))
-    (unless (get-root-object system :id-counter)      
-      ;; create global id
-      (execute system (make-transaction 'tx-create-id-counter)))
-    ;; create model
-    (unless (get-root-object system :model)
-      ;; create a model
-      (execute system (make-transaction 'tx-model-create model-class)))
-    system))
+(defmethod start ((self database-server))
+  "Starts database server"
+  (restore self)
+  (cl-prevalence::open-transaction-log-stream self)
+
+  ;; Create ID counter
+  (unless (get-root-object self :id-counter)
+    (create-id-counter self))
+
+  ;; Set creation date
+  (unless (get-root-object self :creation-date)
+    (set-creation-date self (get-universal-time))))
+
+(defmethod stop ((self database-server))
+  "Stops database server"
+  (when (database-server.status self)
+    (cl-prevalence::close-open-streams self)
+    (setf (cl-prevalence::get-root-objects self) (make-hash-table :test 'eq))
+    (setf (slot-value self 'cl-prevalence::prevalence.serialization-state)
+	  (cl-prevalence::make-serialization-state))))
 
 (defun update-slots (instance lst)
   (let ((clazz (class-of instance))
@@ -171,5 +98,43 @@ model."
                    (cons (filter-object item key) acc))
                lst :initial-value nil))))
 
-(defun make-tx (function &rest args)
-  (apply #'cl-prevalence::make-transaction function (filter-objects args)))
+;; ;;; Convenient Macros
+;; (defun find-free-variables (form)
+;;   (let (lst)
+;;     (mapc (lambda (vars)
+;; 	    (pushnew (name vars) lst))
+;; 	  (ast-search-type form 'free-variable-reference))
+;;     lst))
+
+;; (defmacro with-transaction ((server) &body body)
+;;   (with-unique-names (tx)
+;;     (let* ((form (walk-form `(progn ,@body)))
+;; 	   (vars (filter (lambda (a)
+;; 			   (not (eq server a)))
+;; 			 (find-free-variables form)))
+;; 	   (thunk (walk-form `(lambda (,server ,@vars)
+;; 				,@body))))  
+;;       `(let ((,tx (transaction ,thunk (list ,@vars))))
+;; 	 (execute ,server ,tx)))))
+
+;; ;; (defmacro deftransaction (name args &body body)
+;; ;;   (let ((arg-names (extract-argument-names args :allow-specializers t))
+;; ;; 	(tx (intern (format nil "TX-~A" name))))
+;; ;;     `(progn
+;; ;;        (defun ,tx ,arg-names ,@body)
+;; ;;        (defmethod ,name ,args
+;; ;; 	 (execute ,(car arg-names)
+;; ;; 		  (transaction ',tx (list ,@(cdr arg-names))))))))
+
+;; ;; (defmacro defcrud (class)
+;; ;;   (let ((class+ (find-class+ class)))
+;; ;;     `(progn
+;; ;;        (defmethod find-user ((self database-server) &key ,@(class+.ctor-keywords class+))
+;; ;; 	 ))))
+
+;; ;; (deftransaction find-gee ((self database-server) &key (name nil) (password nil))
+;; ;;   (declare (ignore password))
+;; ;;   (find-object-with-slot self 'muser 'name name))
+
+;; ;; (defclass+ muser ()
+;; ;;   ((name :host local :initarg :name)))
