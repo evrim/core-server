25ba3ed7990d392241dea5f935e41d4a3483865d
evrim <evrim@core.gen.tr>
2008-09-14 02:35:24 +0300

    sysv method combination tests fxied.
    darcs-hash:20080913233524-74ae0-b458addf87415b938f0b8719ec1a58a398e206cc.gz

--- t/method.lisp
+++ t/method.lisp
@@ -2,30 +2,71 @@
 ;; sysv-standard method combination
 
 (defclass a-server (server)
-  ())
+  ((a-server.status :initform nil)))
 
 (defmethod start ((self a-server))
-  (format t "Starting server a~%"))
+  (format t "Starting server a~%")
+  (setf (slot-value self 'a-server.status) t)
+  'a-server-start)
+
+(defmethod stop ((self a-server))
+  (format t "Stopping server a~%")
+  (setf (slot-value self 'a-server.status) nil)
+  'a-server-stop)
+
+(defmethod status ((self a-server))
+  (slot-value self 'a-server.status))
 
 (defclass b-server (server)
-  ())
+  ((b-server.status :initform nil)))
 
 (defmethod start ((self b-server))
-  (format t "Starting server b~%"))
+  (format t "Starting server b~%")
+  'b-server-start)
+
+(defmethod stop ((self b-server))
+  (format t "Stopping server b~%")
+  (setf (slot-value self 'b-server.status) nil)
+  'b-server-stop)
 
 (defclass c-server (a-server)
   ())
 
 (defmethod start ((self c-server))
-  (format t "Starting server c~%"))
+  (format t "Starting server c~%")
+  'c-server-start)
 
-(defclass test-server (c-server b-server)
+(defclass test-server (c-server b-server a-server)
   ())
 
 (defmethod start ((self test-server))
-  (format t "starting server test-server~%"))
+  (format t "starting server test-server~%")
+  'test-server-start)
 
-(start (make-instance 'test-server))
+(deftest sysv-1
+    (let ((s (make-instance 'test-server)))
+      (multiple-value-list (start s)))
+  (test-server-start c-server-start b-server-start a-server-start t))
+
+(deftest sysv-2
+    (let ((s (make-instance 'test-server)))
+      (start s)
+      (multiple-value-list (start s)))
+  (test-server-start c-server-start b-server-start t t))
+
+(deftest sysv-3
+    (let ((s (make-instance 'test-server)))
+      (start s)
+      (multiple-value-list (stop s)))
+  (b-server-stop a-server-stop t))
+
+(deftest sysv-4
+    (let ((s (make-instance 'test-server)))
+      (start s)
+      (stop s)
+      (multiple-value-list (stop s)))
+  (b-server-stop t t))
+
 ;; TEST> (start (make-instance 'core-server::core-server))
 ;; c-server init-instance
 ;; core-server init-instance
