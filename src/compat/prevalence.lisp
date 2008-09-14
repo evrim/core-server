18cb919f1eb7b5c2f63d07f0f75f245b5aee66c1
aycan.irican <aycan.irican@core.gen.tr>
2008-09-14 13:47:03 +0300

    fixed compat layer of prevalence
    darcs-hash:20080914104703-3f8d4-a24a0cdaebf68236102f19e714b43341a4ec7998.gz

--- src/compat/prevalence.lisp
+++ src/compat/prevalence.lisp
@@ -24,38 +24,44 @@
 ;; This file overrides some properties of cl-prevalence database.
 
 (defun cl-prevalence::get-objects-slot-index-name (class &optional (slot 'id))
-  (any (lambda (class)
-	 (if (member slot (mapcar #'slot-definition-name (mopp::class-direct-slots class)))
+  (core-server::any (lambda (class)
+	 (if (member slot (mapcar #'core-server::slot-definition-name (mopp::class-direct-slots class)))
 	     (intern (concatenate 'string (symbol-name (class-name class))
 				  "-" (symbol-name slot) "-INDEX")
 		     :keyword)))
-       (class-superclasses class 'standard-class)))
+       (core-server::class-superclasses class 'standard-class)))
 
 (defun cl-prevalence::class-compile-timestamp (system class)
   (or (get-root-object
-       system (intern (symbol-name (class+.name (find-class+ class))) :keyword))
+       system (intern (symbol-name
+		       (core-server::class+.name
+			(core-server::find-class+ class))) :keyword))
       0))
 
 (defsetf cl-prevalence::class-compile-timestamp (system class) (value)
   `(setf (get-root-object
-	  ,system (intern (symbol-name (class+.name (find-class+ ,class))) :keyword))
+	  ,system (intern (symbol-name
+			   (core-server::class+.name
+			    (core-server::find-class+ ,class))) :keyword))
 	 ,value))
 
 (defun cl-prevalence::class-expired (system class)
-  (let ((class+ (find-class+ class)))
+  (let ((class+ (core-server::find-class+ class)))
     (when class+
-      (if (> (slot-value class+ '%timestamp)
-	     (cl-prevalence::class-compile-timestamp system (class+.name class)))
+      (if (> (slot-value class+ 'core-server::%timestamp)
+	     (cl-prevalence::class-compile-timestamp
+	      system (core-server::class+.name class)))
 	  t))))
 
 (defun cl-prevalence::create-object-slot-indexes (system class)
-  (let ((class+ (find-class+ class)))
+  (let ((class+ (core-server::find-class+ class)))
     (when (and class+ (cl-prevalence::class-expired system class+))
       (mapcar (lambda (slot)
 		(cl-prevalence::tx-create-objects-slot-index system class slot #'equalp))
-	      (mapcar #'slot-definition-name (class+.indexes class+))))
+	      (mapcar #'core-server::slot-definition-name
+		      (core-server::class+.indexes class+))))
       (setf (cl-prevalence::class-compile-timestamp system class)
-	    (slot-value class+ '%timestamp))))
+	    (slot-value class+ 'core-server::%timestamp))))
 
 (defun cl-prevalence::tx-create-object (system class &optional slots-and-values)
   "Create a new object of class in system, assigning it a unique id, optionally setting some slots and values"
@@ -72,3 +78,20 @@
     (tx-change-object-slots system class id slots-and-values)
     (initialize-instance object)
     object))
+
+(defun cl-prevalence::tx-delete-object (system class id)
+  "Delete the object of class with id from the system"
+  (let ((object (find-object-with-id system class id)))
+    (if object
+	(let ((root-name (get-objects-root-name class))
+	      (index-name (get-objects-slot-index-name class 'id)))
+	  (setf (get-root-object system root-name) (delete object (get-root-object system root-name)))
+	  (remhash id (get-root-object system index-name))
+	  (mapcar (lambda (slot)
+		    (remhash (slot-value object slot)
+			     (get-root-object
+			      system (get-objects-slot-index-name class slot))))
+		  (mapcar #'core-server::slot-definition-name
+			  (core-server::class+.indexes
+			   (core-server::find-class+ class)))))
+      (error "no object of class ~a with id ~d found in ~s" class id system))))
