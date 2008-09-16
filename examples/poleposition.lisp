b8d019fb946370b428b3451a7df6453229190831
aycan.irican <aycan.irican@core.gen.tr>
2008-09-16 15:54:43 +0300

    added database performance example
    darcs-hash:20080916125443-3f8d4-e4b2e5e0db7a59073dd13f7034abe2a6429ad6fb.gz

---
+++ examples/poleposition.lisp
@@ -0,0 +1,49 @@
+;; A simple example to demonstrate the performance of prevalence
+;; database
+
+(defpackage :poleposition
+  (:use :cl :core-server :arnesi :cl-prevalence))
+
+(in-package :poleposition)
+
+(defclass+ pilot ()
+  ((name)
+   (firstname)
+   (points :index t)
+   (licenseid)))
+
+(defcrud pilot)
+
+(defparameter *db*
+  (make-instance 'database-server
+		 :directory #P"/tmp/poleposition/"
+		 :auto-start t))
+
+;;;; Executing body in one transaction
+(defun pp-write (count)
+  (with-transaction (*db*)
+    (loop
+       for i from 1 to count
+       do (pilot-add *db* :name (format nil "Pilot_~D" i) :firstname "Herkules" :points i :licenseid i))))
+
+(defun pp-read (count)
+  (loop
+     for i from 1 to count
+     do (find-pilot *db* :points i)))
+
+(defun pp-delete (count)
+  (let ((sid (get-id (find-pilot *db* :points 1))))
+   (with-transaction (*db*)
+     (loop
+	for i from sid to count
+	do (cl-prevalence:tx-delete-object *db* 'pilot i)))))
+
+(defun test ()
+  (totally-destroy *db*)
+  (start *db*)
+  (format t "* Creating 30000 objects~%")
+  (time (pp-write 30000))
+  (format t "* Reading 30000 objects~%")
+  (time (pp-read 30000))
+  (format t "* Deleting 30000 objects~%")
+  (time (pp-delete 30000)))
