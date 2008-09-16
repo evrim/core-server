7040b3500768e220ca1320848e679a5c57e43b3e
evrim <evrim@core.gen.tr>
2008-09-16 05:15:09 +0300

    fckeditor moved to editor.lisp
    darcs-hash:20080916021509-74ae0-961fcfc9cf1fe79add69bfe200a40bd8ad491fdd.gz

--- src/web/editor.lisp
+++ src/web/editor.lisp
@@ -1,34 +1,19 @@
-;; Core Server: Web Application Server
-
-;; Copyright (C) 2006-2008  Metin Evrim Ulu, Aycan iRiCAN
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+;; +----------------------------------------------------------------------------
+;; | HTML Editors
+;; +----------------------------------------------------------------------------
 (in-package :core-server)
 
-(defvar +fck-image-extensions+
-  '("bmp" "gif" "jpeg" "jpg" "png" "psd" "tif" "tiff"))
-
-(defvar +fck-flash-extensions+
-  '("swf" "fla"))
-
+;; ----------------------------------------------------------------------------
+;; Fck Editor
+;; ----------------------------------------------------------------------------
+(defvar +fck-image-extensions+ '("bmp" "gif" "jpeg" "jpg" "png" "psd" "tif" "tiff"))
+(defvar +fck-flash-extensions+ '("swf" "fla"))
 (defvar +fck-media-extensions+
   '("aiff" "asf" "avi" "bmp" "fla" "flv" "gif" "jpeg" "jpg"
     "mid" "mov" "mp3" "mp4" "mpc" "mpeg" "mpg" "png" "qt"
     "ram" "rm" "rmi" "rmvb" "swf" "tif" "tiff" "wav" "wma" "wmv"))
 
-;; TODO: fix this shit!
+;; TODO: fix this
 (defun/cc handle-fck-browse (path publish-path)
   (let ((stream (http-response.stream (context.response +context+))))
     (labels ((send-error (number &optional message)
@@ -201,3 +186,20 @@
 	  config.*image-upload false
 	  config.*flash-upload false)
     (return fck)))
+
+;; Core Server: Web Application Server
+
+;; Copyright (C) 2006-2008  Metin Evrim Ulu, Aycan iRiCAN
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
