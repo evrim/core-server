48bc8b2aade36e34988f29a299bf993e09f6c878
evrim <evrim@core.gen.tr>
2008-11-20 12:24:29 +0200

    servers upgraded.
    darcs-hash:20081120102429-74ae0-67626690269c598ceeb3938268b686c490e6788c.gz

--- src/servers/http.lisp
+++ src/servers/http.lisp
@@ -1,23 +1,96 @@
-;; Core Server: Web Application Server
-
-;; Copyright (C) 2006-2008  Metin Evrim Ulu, Aycan iRiCAN
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 (in-package :core-server)
 
-(defmethod render-404 ((request http-request) (response http-response))
+;; +----------------------------------------------------------------------------
+;; | HTTP Server Implementation
+;; +----------------------------------------------------------------------------
+(defvar +default-encoding-for-remote-mimes+
+  :utf-8 "FIXME: Browser shoudl supply in which encoding did it encode data.")
+
+(defmethod/cc2 parse-request ((server http-server) (stream core-stream))
+  "Returns a fresh RFC 2616 HTTP Request object parsing from 'stream',
+nil if stream data is invalid"
+  (multiple-value-bind (peer-type method uri version general-headers
+				  request-headers entity-headers unknown-headers)
+      (http-request-headers? stream)
+    (if method
+	(let ((request (make-instance 'http-request :stream stream)))
+	  (let ((content-type (cadr (assoc 'content-type entity-headers)))
+		(content-length (cadr (assoc 'content-length entity-headers))))
+	    (if (null content-length)
+		(setf content-length 0))
+	    (cond
+	      ;; content-type = '("multipart" "form-data")
+	      ((and (string-equal "multipart" (car content-type))
+		    (string-equal "form-data" (cadr content-type))
+		    (> content-length 0))
+	       ;; eat lineer whayt spaces.
+	       (lwsp? stream)
+	       ;; set max-read to content-length for termination
+	       (setf (slot-value stream '%max-read) (1- content-length))
+	       (setf (http-message.entities request)
+		     (rfc2388-mimes?
+		      stream
+		      (cdr
+		       (assoc "boundary" (caddr content-type) :test #'string=))))
+	       (setf (uri.queries uri)
+		     (append (uri.queries uri)
+			     (reduce0 (lambda (acc media)						     
+					(cond
+					  ((mime.filename media)
+					   (cons (cons (mime.name media) media)
+						 acc))
+					  ((mime.name media)
+					   (cons (cons (mime.name media)
+						       (octets-to-string (mime.data media)
+									 +default-encoding-for-remote-mimes+))
+						 acc))
+					  (t
+					   (warn "Unkown media received:~A" media)
+					   acc)))
+				      (filter (lambda (a)
+						(typep a 'top-level-media))
+					      (http-message.entities request))))))
+	      ((and (string-equal "text" (car content-type))
+		    (string-equal "json" (cadr content-type)))
+	       (let ((hash-table (json? stream)))
+		 (if (typep hash-table 'hash-table)
+		     (setf (slot-value stream '%max-read) (1- content-length)
+			   (uri.queries uri) (append (uri.queries uri)
+						     (hash-to-alist hash-table))))))
+	      ;; content-type = '("application" "x-www-form-urlencoded")
+	      ((and (string-equal "application" (car content-type))
+		    (string-equal "x-www-form-urlencoded" (cadr content-type))
+		    (>  content-length 0))
+	       ;; eat lineer whayt spaces.
+	       (lwsp? stream)
+	       ;; set max-read to content-length for termination	       
+	       (setf (slot-value stream '%max-read) (1- content-length)
+		     (uri.queries uri) (append (uri.queries uri)					       
+					       (x-www-form-urlencoded? stream))))))
+	  (setf (http-message.general-headers request) general-headers
+		(http-message.unknown-headers request) unknown-headers
+		(http-request.headers request) request-headers
+		(http-request.entity-headers request) entity-headers
+		(http-request.uri request) uri
+		(http-request.method request) method
+		(http-message.version request) version
+		(http-request.peer-type request) peer-type)
+	  request))))
+
+(defun make-response (&optional (stream (make-core-stream "")))
+  "Returns an empty HTTP Response object"
+  (let ((response (make-instance 'http-response :stream stream)))
+    (setf (http-message.general-headers response)
+	  (list (cons 'date (get-universal-time))
+		;; (list 'pragma 'no-cache)
+		(cons 'connection 'keep-alive))
+	  (http-response.entity-headers response)
+ 	  (list (cons 'content-type (list "text" "html" (list "charset" "UTF-8"))))
+	  (http-response.response-headers response)
+	  (list (cons 'server  "Core-serveR - www.core.gen.tr")))
+    response))
+
+(defmethod/cc2 render-404 ((server http-server) (request http-request) (response http-response))
   (setf (http-response.status-code response) (make-status-code 404))
   (rewind-stream (http-response.stream response))
   (checkpoint-stream (http-response.stream response))
@@ -27,31 +100,83 @@
       "Core-serveR - URL Not Found")))
   response)
 
-(defmethod eval-request ((self custom-http-peer) (request http-request))
-  (let ((response (call-next-method))
+(defmethod/cc2 eval-request ((server http-server) (request http-request))
+  (let ((response (make-response ;; (http-request.stream request)
+				 ;; (make-core-stream (slot-value (http-request.stream request) '%stream))
+				 ))
 	(host (caadr (assoc 'HOST (http-request.headers request))))
 	(app-name (caar (uri.paths (http-request.uri request)))))
+
+    (checkpoint-stream (http-response.stream response))
+    
     (cond
       ;; dispatch by app-name like http://servername/app-fqdn/js.core
       ((any #'(lambda (app)
 		(when (string= app-name (web-application.fqdn app)) 
 		  (pop (uri.paths (http-request.uri request)))
 		  (dispatch app request response)))
-	    (server.applications (peer.server self)))
+	    (server.applications server))
        response)
       ;; dispatch by hostname like http://servername/js.core
       ((and (stringp host)
 	    (any #'(lambda (app)
 		     (when (string= host (web-application.fqdn app)) 
 		       (dispatch app request response)))
-		 (server.applications (peer.server self))))
+		 (server.applications server)))
        response)
       ;; catch-all via 404
       (t
        (progn
-	 (log-me (peer.server self) 'http-peer
+	 (log-me server 'eval-request
 		 (format nil "request uri: ~A" (http-request.uri request)))
-	 (render-404 request response))))))
+	 (render-404 server request response))))))
+
+(defmethod/cc2 render-error ((self http-server) stream)
+  "Renders a generic server error response to 'stream'"
+  (let ((response (make-response stream)))
+    (checkpoint-stream stream)
+    (setf (http-response.status-code response) (make-status-code 500))
+    (http-response-headers! stream response)
+    (char! stream #\Newline)
+    (with-html-output stream (<:html (<:body "An error condition occured and ignored.")))
+    (commit-stream stream)))
+
+(defmethod/cc2 render-response ((self http-server) response request)
+  "Renders HTTP/Mod-Lisp 'response' to 'stream'"
+  (let ((content-length (length (slot-value (http-response.stream response) '%buffer)))
+	(stream (http-request.stream request)))
+    (http-response.add-entity-header response 'content-length content-length)
+
+    (cond
+      ((eq 'mod-lisp (http-request.peer-type request))
+       ;; Renders Mod-Lisp HTTP 'response' to 'stream'
+       (mod-lisp-response-headers! stream response)
+       (string! stream "end")
+       (char! stream #\Newline))
+      (t
+       ;; Renders RFC 2616 HTTP 'response' to 'stream'
+       (http-response-headers! stream response)
+       (char! stream #\Newline)
+       ))
+    
+    ;;     (describe stream)
+    ;;     (commit-stream stream)
+    ;;     (if (not (eq 'head (http-request.method request)))
+    ;; 	(commit-stream (http-response.stream response)))
+    ;;     (write-stream stream "HTTP/1.1 200 OK
+    ;; DATE: Thu, 30 Nov 2008 17:58:12 GMT
+    ;; CONNECTION: keep-alive
+    ;; SERVER: Core-serveR - www.core.gen.tr
+    ;; CONTENT-LENGTH: 5192
+    ;; CONTENT-TYPE: text/html
+
+    ;; ")
+    ;;     (write-stream stream *5k*)
+    (when (not (eq 'head (http-request.method request)))
+      (write-stream stream (slot-value (http-response.stream response) '%buffer)))
+    ;; (describe stream)
+    ))
+
 
 ;;-----------------------------------------------------------------------------
 ;; Server Protocol Implementation
@@ -73,3 +198,235 @@
 (defmethod find-application ((self http-server) fqdn)
   (find fqdn (server.applications self) :key #'web-application.fqdn :test #'equal))
 
+;; +----------------------------------------------------------------------------
+;; | HTTP Unit
+;; +----------------------------------------------------------------------------
+(defclass http-unit (stream-peer)
+  ()
+  (:default-initargs :name "Http Peer Handling Unit")
+  (:documentation "HTTP Peer - This peer handles HTTP requests and
+evaulates to a HTTP response. Its' server is an instance of http-server"))
+
+(defmethod/unit handle-stream :async-no-return ((peer http-unit) (stream core-stream) address)
+  (flet ((handle-error (condition)
+	   (if (typep condition 'sb-int::simple-stream-error)    
+	       (return-from handle-stream nil))
+	   (flet ((ignore-error (unit)
+		    (render-error unit stream)
+		    (close-stream stream))
+		  (retry-error (unit)
+		    (do ((i (current-checkpoint stream)
+			    (current-checkpoint stream)))
+			((< (the fixnum i) 0) nil)
+		      (rewind-stream stream))
+		    (handle-stream unit stream address)))
+	     (debug-condition (aif (slot-value peer '%debug-unit) it peer)
+			      condition
+			      (lambda () (send-message peer #'ignore-error))
+			      (lambda () (send-message peer #'retry-error)))
+	   (return-from handle-stream nil))))
+    (handler-bind ((error #'handle-error))
+      (checkpoint-stream stream)
+      (let ((request (parse-request (peer.server peer) stream)))
+	(if request
+	    (let ((response (eval-request (peer.server peer) request)))
+	      (if response
+		  (render-response (peer.server peer) response request))))
+	(close-stream stream)))))
+
+(defclass http-cps-unit (local-unit peer)
+  ((%max-events :initarg :max-events :initform 100)
+   (%timeout :initarg :timeout :initform 10)
+   (%continuations :initform (make-hash-table))
+   (%epoll-fd :initform nil)
+   (%epoll-size :initarg :epoll-size :initform 131072)
+   (%stream :initform (make-instance 'core-cps-stream :stack (list)))
+   (%receive-messages :initform t)))
+
+(defmethod start ((self http-cps-unit))
+  (setf (s-v '%epoll-fd) (core-ffi::epoll-create (s-v '%epoll-size))))
+
+(defmethod start ((self http-server))
+;; ;;   (thread-kill (s-v '%socket-thread))
+;; ;;   (setf (s-v '%socket-thread) nil)
+;;   (let ((listenfd (socket-file-descriptor (s-v '%socket))))
+;;     (core-ffi::set-nonblock listenfd)
+;;     (handle-accept (car (s-v '%peers)) listenfd (s-v '%peers)))
+  )
+
+(defmethod/unit add-fd ((self http-cps-unit) fd modes k)
+  (setf (gethash fd (s-v '%continuations)) k)
+  (with-foreign-object (event 'core-ffi::epoll-event)
+    (core-ffi::bzero event core-ffi::size-of-epoll-event)
+    (with-foreign-slots ((core-ffi::events core-ffi::data) event core-ffi::epoll-event)
+      (setf core-ffi::events (apply #'logior (ensure-list modes))
+	    (foreign-slot-value
+	     (foreign-slot-value event 'core-ffi::epoll-event 'core-ffi::data)
+	     'core-ffi::epoll-data 'core-ffi::fd) fd))
+    (core-ffi::epoll-ctl (s-v '%epoll-fd) core-ffi::epoll-ctl-add fd event)))
+
+(defmethod/unit del-fd ((self http-cps-unit) fd)
+  (core-ffi::epoll-ctl (s-v '%epoll-fd) core-ffi::epoll-ctl-del fd (null-pointer)))
+
+(defmethod receive-messages ((self http-cps-unit))
+  (let ((thread (or (s-v '%thread) (current-thread))))
+    (let* ((mbox (thread-mailbox thread))
+	   (lock (mailbox.lock mbox)))
+      (with-lock-held (lock)
+	(loop
+	   (let ((q (mailbox.queue mbox)))
+	     (setf (mailbox.queue mbox) '())
+	     (return q)))))))
+
+(defmethod receive-events ((self http-cps-unit))
+  (when (s-v '%epoll-fd)
+    (with-foreign-object (events 'core-ffi::epoll-event (s-v '%max-events))
+      (core-ffi::bzero events (* (s-v '%max-events) core-ffi::size-of-epoll-event))
+      (labels ((collect (i &optional (acc nil))
+		 (if (< i 0)
+		     acc
+		     (collect (1- i) (cons
+				      (foreign-slot-value
+				       (foreign-slot-value
+					(mem-ref events 'core-ffi::epoll-event (1- i))
+					'core-ffi::epoll-event 'core-ffi::data)
+				       'core-ffi::epoll-data 'core-ffi::fd) acc)))))
+	(let ((n (core-ffi::epoll-wait (s-v '%epoll-fd) events (s-v '%max-events)
+				       (if (s-v '%receive-messages)
+					   (s-v '%timeout)
+					   -1))))
+	  (if (> n 0)
+	      (collect n)
+	      nil))))))
+
+(defmethod run ((self http-cps-unit))
+  (handler-bind ((error (lambda (condition)
+			  (let ((swank::*sldb-quit-restart* 'ignore-error))
+			    (restart-case (swank:swank-debugger-hook condition nil)
+			      (ignore-error ()
+				:report "Ignore the error and return (values)")
+			      (retry ()
+				:report "Retry the funcall"))))))
+    (flet ((process-message (message)	  	   
+	     (cond
+	       ((eq message 'shutdown) (return-from run nil))
+	       ((functionp message) (funcall message self))
+	       (t (format *standard-output* "Uniw ~A got unkown message:~A~%" self message))))
+	   (process-event (fd)
+	     (let ((k (gethash fd (s-v '%continuations))))
+	       (when k
+		 (remhash k (s-v '%continuations))
+		 (let ((+stream+ (s-v '%stream)))
+		   (setf (slot-value +stream+ '%stack) (list k))
+		   (run +stream+))))))
+      (loop
+	 (progn
+	   (if (s-v '%receive-messages) (mapc #'process-message (receive-messages self)))
+	   (mapc #'process-event (receive-events self)))))))
+
+(defmethod/cc1 handle-stream4 ((peer http-cps-unit) (stream core-fd-nio-stream) address)
+;;   (describe (current-checkpoint stream))
+;;   (checkpoint-stream stream)
+  (let ((time (get-universal-time)))
+    ;; (let ((request (parse-request (peer.server peer) stream)))
+;;       ;; (if request
+;; ;; 	  (let ((response (eval-request (peer.server peer) request)))
+;; ;; 	    (if response
+;; ;; 		(render-response (peer.server peer) response request))))
+;;       (if request
+;; 	  (render-response (peer.server peer) (make-response) request))
+;;       (close-stream stream)
+;;       (push (- (get-universal-time) time) *timing*))
+    (write-stream stream "HTTP/1.1 200 OK
+DATE: Thu, 30 Nov 2008 17:58:12 GMT
+CONNECTION: keep-alive
+SERVER: Core-serveR - www.core.gen.tr
+CONTENT-LENGTH: 5192
+CONTENT-TYPE: text/html
+
+")
+;;     (write-stream stream *5k*)
+    (write-content stream)
+    (close-stream stream)
+    ;; (render-response (peer.server peer) (make-response) nil)
+    ))
+
+(defmethod/unit handle-stream :async-no-return ((peer http-cps-unit) (stream core-stream) address)
+  (let ((fd (sb-sys::fd-stream-fd (slot-value stream '%stream))))
+    (add-fd peer fd (list core-ffi::epollin #.(ash 1 31))
+	    (lambda (&rest values)
+	      (declare (ignore values))
+	      (handle-stream4 peer
+			      (make-instance 'core-fd-nio-stream
+					     :stream fd
+					     :unit peer)
+			      address)))))
+
+(defmethod/unit handle-stream :async-no-return ((peer http-cps-unit) (stream number) address)
+  (let ((fd stream))
+    (add-fd peer fd (list core-ffi::epollin #.(ash 1 31))
+	    (lambda (&rest values)
+	      (declare (ignore values))
+	      (handle-stream4 peer
+			      (make-instance 'core-fd-nio-stream
+					     :stream fd
+					     :unit peer)
+			      address)))))
+
+(defmethod/unit handle-accept :async-no-return ((self http-cps-unit) listenfd peers)
+  (let* ((peers (copy-list peers))
+	 (peers2 (copy-list peers)))
+    (labels ((kont (&rest values)
+	       (declare (ignore values))
+	       (multiple-value-bind (fd address) (core-ffi::accept listenfd)
+		 (handle-stream self fd address)
+		 ;; (if (car peers)
+;; 		     (progn
+;; 		       (handle-stream (car peers) fd address)
+;; 		       (setq peers (cdr peers))
+;; 		       ;; reschedule
+;; 		       (setf (gethash listenfd (s-v '%continuations)) #'kont))
+;; 		     (progn
+;; 		       (setq peers peers2)
+;; 		       (kont values)))
+		 )))
+      (add-fd (car peers) listenfd (list core-ffi::epollin #.(ash 1 31)) #'kont)
+      (setf (s-v '%receive-messages) nil))))
+
+(deftrace http-server
+    '(handle-stream dispatch render-error render-response eval-request parse-request
+      handle-stream4 run add-fd del-fd ;; receive-messages
+      receive-events
+    ;;   render-headers
+      make-response;;  render-http-headers
+;;       render-mod-lisp-headers
+      ))
+
+;; Core Server: Web Application Server
+
+;; Copyright (C) 2006-2008  Metin Evrim Ulu, Aycan iRiCAN
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; (mapcar #'(lambda (mime)
+;;; 		      (describe mime)
+;;; 		      (when (and (mime.filename mime) (mime.data mime))
+;;; 			(with-core-stream (stream (pathname (concatenate 'string "/tmp/" (mime.filename mime)))) 
+;;; 			  (describe (length (mime.data mime)))
+;;; 			  (reduce #'(lambda (s a)
+;;; 				      (prog1 s (write-stream s a)))
+;;; 				  (mime.data mime) :initial-value stream))))
+;;; 		  (http-message.entities request))
+	  ;;	  (describe (mime-part.data (nth 3 (http-message.entities request))))
--- src/servers/socket.lisp
+++ src/servers/socket.lisp
@@ -37,16 +37,19 @@
 ;;-----------------------------------------------------------------------------
 ;; Server Procotol Implementation
 ;;-----------------------------------------------------------------------------
+(defmethod make-bind-socket ((self socket-server))
+  (make-server :host (s-v 'host)
+	       :port (s-v 'port)
+	       :reuse-address (s-v 'reuse-address)
+	       :backlog (s-v 'backlog)
+	       :protocol (s-v 'protocol)))
+
 (defmethod start ((self socket-server))
   (when (not (socket-server.status self))
     (when (null (s-v 'debug))
       (setf (s-v '%debug-unit) (make-instance 'local-unit :name "Http Server Debugging Unit"))
       (start (s-v '%debug-unit)))
-    (setf (s-v '%socket) (make-server :host (s-v 'host)
-				      :port (s-v 'port)
-				      :reuse-address (s-v 'reuse-address)
-				      :backlog (s-v 'backlog)
-				      :protocol (s-v 'protocol))
+    (setf (s-v '%socket) (make-bind-socket self)
 	  (s-v '%peers) (mapcar
 			 #'(lambda (n)
 			     (declare (ignore n))
@@ -72,7 +75,7 @@
 
 (defmethod stop ((self socket-server))
   (when (socket-server.status self)
-    (thread-kill (s-v '%socket-thread))
+    (and (s-v '%socket-thread) (thread-kill (s-v '%socket-thread)))
     (and (s-v '%socket) (close-server (s-v '%socket)))
     (mapc #'stop (s-v '%peers))
     (stop (s-v '%debug-unit))
@@ -85,3 +88,93 @@
 
 (defmethod status ((self socket-server))
   (socket-server.status self))
+
+;; +----------------------------------------------------------------------------
+;; | Non Blocking Socket IO Server
+;; +----------------------------------------------------------------------------
+;; (defclass nio-socket-server (local-unit)
+;;   ((host :initarg :host :initform "0.0.0.0"
+;; 	 :documentation "IP address that this server binds to")
+;;    (port :initarg :port :initform 3009
+;; 	 :documentation "Port that this server binds to")
+;;    (protocol :initarg :protocol :initform :tcp
+;; 	     :documentation "Network Protocol, can be :udp, :tcp")
+;;    (reuse-address :initarg :reuse-address :initform t
+;; 		  :documentation "TCP reuse address option")
+;;    (backlog :initarg :backlog :initform 1
+;; 	    :documentation "TCP backlog option")
+;;    (peers-max :initarg :peers-max :initform 6
+;; 	      :documentation "Number of peers that this server manages")
+;;    (element-type :initarg :element-type :initform '(unsigned-byte 8)
+;; 		 :documentation "Data type for socket stream")
+;;    (external-format :initarg :external-format :initform :utf-8
+;; 		    :documentation "External format for stream")
+;;    (peer-class :initarg :peer-class :initform 'stream-peer
+;; 	       :documentation "Class to instantiate as peer thread.")
+;;    ;;   (request-timeout-length :initarg :request-timeout-length :initform 90)
+;;    (%socket :initform nil) (%peers :initform nil)
+;;    (%debug-unit :initform nil))
+;;   (:documentation "Non blocking socket server"))
+
+;; (defmethod start ((self nio-socket-server))  
+;;   (when (null (s-v 'debug))
+;;     (setf (s-v '%debug-unit) (make-instance 'local-unit :name "Http Server Debugging Unit"))
+;;     (start (s-v '%debug-unit)))
+
+  
+;;   (setf (s-v '%socket) (core-ffi::bind (s-v '%host) (s-v '%port))
+;; 	(s-v '%peers) (mapcar
+;; 		       #'(lambda (n)
+;; 			   (declare (ignore n))
+;; 			   (let ((p (if (listp (s-v 'peer-class))
+;; 					(apply #'make-instance
+;; 					       (car (s-v 'peer-class))
+;; 					       (cons :debug-unit
+;; 						     (cons (if (null (s-v 'debug))
+;; 							       (s-v '%debug-unit)
+;; 							       nil)
+;; 							   (cdr (s-v 'peer-class)))))
+;; 					(make-instance (s-v 'peer-class)
+;; 						       :debug-unit (if (null (s-v 'debug))
+;; 								       (s-v '%debug-unit)
+;; 								       nil)))))
+;; 			     (start p)
+;; 			     (setf (peer.server p) self)
+;; 			     p))
+;; 		       (seq (s-v 'peers-max)))
+;; 	(s-v '%socket-thread) (thread-spawn #'(lambda () (socket-server.run self))
+;; 					    :name (format nil "Socket Server at ~A:~A"
+;; 							  (s-v 'host) (s-v 'port)))))
+
+
+;; (defmethod stop ((self socket-server))
+;;   (when (socket-server.status self)
+;;     (thread-kill (s-v '%socket-thread))
+;;     (and (s-v '%socket) (close-server (s-v '%socket)))
+;;     (mapc #'stop (s-v '%peers))
+;;     (stop (s-v '%debug-unit))
+;;     (setf (s-v '%socket) nil
+;; 	  (s-v '%socket-thread) nil
+;; 	  (s-v '%peers) nil)))
+
+
+;; (defmethod run ((self nio-socket-server))
+;;   )
+
+;; (defmethod make-bind-socket ((self nio-socket-server))
+;;   (nio-make-server :host (s-v 'host)
+;; 		   :port (s-v 'port)
+;; 		   :reuse-address (s-v 'reuse-address)
+;; 		   :backlog (s-v 'backlog)
+;; 		   :protocol (s-v 'protocol)))
+
+;; (defmethod socket-server.run ((self nio-socket-server))
+;;   "Run method of Socket Server"
+;;   (loop
+;;      (progn
+;;        (mapc #'(lambda (peer)
+;; 		 (multiple-value-bind (stream address) (nio-accept (s-v '%socket))
+;; 		   (when stream
+;; 		     (if (not (status peer)) (start peer))
+;; 		     (handle-stream peer stream address))))
+;; 	     (s-v '%peers)))))
