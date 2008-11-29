7b578324fb7641105e6fd90e4e447a1f749a1d0a
aycan.irican <aycan.irican@core.gen.tr>
2008-11-29 15:00:11 +0200

    added relational object example.
    Ignore-this: 43af80c9079d352b9c1783668c0d39cf
    
    darcs-hash:20081129130011-3f8d4-59c0a33de41d24b5b73911587107c26b8616c14e.gz

---
+++ examples/relations.lisp
@@ -0,0 +1,40 @@
+;; A simple example demonstrating relations between objects
+
+(defpackage :relations
+  (:use :cl :core-server :arnesi))
+
+(in-package :relations)
+
+(defclass+ category (object-with-id)
+  ((objects :type cobject* :relation category)
+   (label :print t)))
+
+(defclass+ cobject (object-with-id)
+  ((category :type category :relation objects)
+   (name :print t)))
+
+(defcrud category)
+(defcrud cobject)
+
+(defparameter *db*
+  (make-instance 'database-server
+		 :database-directory #P"/tmp/relations/"
+		 :auto-start t))
+
+(defun make-new-category (db label objs)
+  (let ((cat (category-add db :label label)))
+    (mapcar #'(lambda (c)
+                (cobject-add db :name c :category cat))
+            objs)))
+
+(defun test (db)
+  (start db)
+  (make-new-category db "Alphabet" (list #\A #\B #\C #\D #\E #\F))
+  (make-new-category db "Numbers" (list 1 2 3 4 5 6))
+  (stop db))
+
+(defun list-db (db)
+  (start db)
+  (mapcar #'describe (cobject-list db))
+  (stop db))
+
