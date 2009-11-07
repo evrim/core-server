b5d88a41001158444b465dff9c4f6b2f4c261a3e
evrim <evrim@core.gen.tr>
2009-11-07 02:56:29 +0200

    trying to fix <core:crud
    darcs-hash:20091107005629-74ae0-0985bae1813aa50b2de9f8a11491c9295c4a3f0e.gz

--- src/web/crud.lisp
+++ src/web/crud.lisp
@@ -1,29 +1,51 @@
 (in-package :core-server)
 
 (defcomponent <core:crud (<:div)
-  ((instance :host remote :initform nil)))
+  ((instance :host none :initform nil)
+   (template-class :host none :initform nil)))
+
+(defmethod/local _get-instance ((component <core:crud))
+  (if (slot-value component 'instance)
+      (with-slots (instance template-class) component
+	(let ((template-class (if (symbolp template-class) (class+.find template-class) template-class)))
+	  (object->jobject (slot-value component 'instance)
+			   template-class)))
+      nil))
+
+(defmethod/remote get-instance ((component <core:crud))
+  (if (slot-value component 'instance)
+      (slot-value component 'instance)
+      (setf (slot-value component 'instance) (_get-instance component))))
 
 (defmethod/remote get-class ((component <core:crud))
-  (slot-value (instance self) 'class))
+  (slot-value (slot-value component 'instance) 'class))
 
 (defmethod/remote get-type ((component <core:crud) field)
-  (slot-value (slot-value (get-class self) field) 'type))
+  (slot-value (slot-value (get-class component) field) 'type))
+
+(defmacro/js instance (crud)
+  `(slot-value ,crud 'instance))
 
 (defmethod/remote get-value ((component <core:crud) field)
   (if (eq "password" (get-type component field))
       "*****************"
-      (slot-value (instance self) field)))
-
-(defmethod/remote set-value ((component <core:crud) field value)
-  (setf (slot-value (instance component) field)value)
-  ;; (update-slot component field value)
-  value)
+      (slot-value (instance component) field)))
 
 (defmethod/local update-slot ((component <core:crud) slot-name value)
-  (answer (cons slot-name value)))
+  (if (instance component)
+      (answer
+       (cons
+	(slot-definition-name
+	 (class+.find-slot (template-class component) (string-upcase slot-name)))
+	value))
+      nil))
+
+(defmethod/remote set-value ((component <core:crud) field value)
+  (setf (slot-value (instance component) field) value)
+  (answer-component component (update-slot component field value)))
 
 (defmethod/remote edit-me ((component <core:crud) slot)
-  (let ((value (get-value self (slot-value slot 'name))))
+  (let ((value (get-value component (slot-value slot 'name))))
     (with-slots (name type) slot
       (cond
 	((eq type "password")
@@ -32,7 +54,7 @@
 	 (<:input :type "text" :name name :value value))))))
 
 (defmethod/remote view-me ((component <core:crud) slot)
-  (let ((value (get-value self (slot-value slot 'name))))
+  (let ((value (get-value component (slot-value slot 'name))))
     (<:a :href "#"
 	 (if (null value)
 	     (+ "Click to set " (slot-value slot 'name))
@@ -46,53 +68,50 @@
 		(let ((view (view-me component slot))
 		      (edit (edit-me component slot)))
 		  (labels ((edit-it ()
-			     (console.debug "edit")
+			     (debug "edit")
 			     (show edit)
 			     (hide view)
-			     (console.debug edit)
-			     (make-web-thread
-			      (lambda ()
-				(try
-				 (let ((focus (slot-value edit 'focus)))
-				   (console.debug focus)
-				   (focus))
-				 (:catch (e) (console.debug e))))))
+			     (let ((focus (slot-value edit 'focus)))
+			       (focus)))
 			   (save-it ()
-			     (console.debug "save")
-			     (set-value component name (slot-value edit 'value))
-			     (setf (slot-value view 'inner-h-t-m-l) (get-value component name))
+			     (debug "save")
+			     (setf (slot-value view 'inner-h-t-m-l) (slot-value edit 'value))
 			     (show view)
-			     (hide edit))
+			     (hide edit)
+			     (set-value component name (slot-value edit 'value)))
 			   (cancel-it ()
-			     (console.debug "cancel")
-			     (setf (slot-value edit 'value) (get-value self name))
+			     (debug "cancel")
+			     (setf (slot-value edit 'value) (get-value component name))
 			     (show view)
 			     (hide edit)))
 		    (hide edit)
 		    (setf (slot-value view 'onclick)
-			  (event (e)
-			    (with-call/cc (call/cc edit-it))
+			  (lambda (e)
+			    (make-web-thread edit-it)
 			    ;; (setf edit.onblur (lambda (e) (save-it)))
 			    false)
 			  (slot-value edit 'onkeydown)
-			  (event (e)
-			    (with-call/cc
-			      (let ((keycode e.key-code;; (if window.event
-					     ;; 	 window.event.key-code
-					     ;; 	 e.key-code)
-				      ))
-				(cond
-				  ((eq 27 keycode) ;; escape
-				   (call/cc cancel-it))
-				  ((eq 13 keycode) ;; enter
-				   (call/cc save-it))
-				  (t
-				   (console.debug (+ "key:" keycode))))))
+			  (lambda (e)
+			    (let ((keycode (slot-value e 'key-code)))
+			      (cond
+				((eq 27 keycode) ;; escape
+				 (make-web-thread cancel-it)
+				 false)
+				((eq 13 keycode) ;; enter
+				 (make-web-thread save-it)
+				 false)
+				(t
+				 (debug (+ "key:" keycode))
+				 true)))
 			    ;; 					  (setf edit.onblur (lambda (e) false))
-			    true))
+			    ))
 		    (with-field label (list view edit))))))
 	    (get-class component)))
    (<:p (<:b "Note: ") "Click on the field to modify, press enter to save or escape to cancel.")))
 
 (defmethod/remote init ((component <core:crud))
-  (.append-child component (template component)))
+  (get-instance component)
+  (let ((template (template component)))
+    (setf (slot-value component 'inner-h-t-m-l) nil)
+    (.append-child component template))
+  component)
