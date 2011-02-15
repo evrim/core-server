fd58f7509aea3b378354cd75178772be46f2516f
evrim <evrim@core.gen.tr>
2011-02-15 21:16:17 +0200

    export title
    darcs-hash:20110215191617-74ae0-18c0d1932756595bd08c968bfad09e164dd05f88.gz

--- src/core-server.lisp
+++ src/core-server.lisp
@@ -918,6 +918,7 @@
    #:forgot-password-dialog
    #:prompt-dialog
    #:big-dialog
+   #:title
    ;; #:template
    #:get-message
    #:buttons
@@ -1056,11 +1057,13 @@
 	   #:map #:meta #:noframes #:noscript #:object #:ol #:optgroup #:option
 	   #:p #:param #:pre #:q #:samp #:script #:select #:small #:span #:strong
 	   #:style #:sub #:sup #:table #:tbody #:td #:textarea #:tfoot #:th #:thead
-	   #:title #:tr #:tt #:ul #:var #:embed #:foo #:bar))
+	   #:title #:tr #:tt #:ul #:var #:embed #:foo #:bar)
+  (:shadowing-import-from #:core-server #:title))
 
 (defpackage :tr.gen.core.server.rss
   (:nicknames :<rss :core-server.rss)
-  (:use :core-server))
+  (:use :core-server)
+  (:shadowing-import-from #:core-server #:title))
 
 (defpackage :tr.gen.core.server.tags
   (:nicknames :<core)
