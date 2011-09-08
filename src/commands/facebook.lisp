4102b9e7d8a07ff6b4c00e09a2573b45a81b14f8
evrim <evrim@core.gen.tr>
2011-09-08 08:41:42 +0300

    facebook <fb:authenticate
    Ignore-this: 8e11e40914feca5a88ecde90db50c7e8
    
    darcs-hash:20110908054142-74ae0-9712ea5539c0e715041dc769c0298745bb2eb745.gz

--- src/commands/facebook.lisp
+++ src/commands/facebook.lisp
@@ -8,3 +8,9 @@
 (defmethod run ((self <fb:me))
   (http.add-query self "access_token" (s-v 'access_token))
   (call-next-method self))
+
+(defun <fb:authenticate (token)
+  (let ((user (apply #'jobject (<fb:me :access_token token))))
+    (list :email (format nil "~A@facebook.com"
+			 (get-attribute user :username))
+	  :name (get-attribute user :name))))
