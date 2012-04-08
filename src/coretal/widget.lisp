559b94fee1aa3e6bf6f5813626c6f48cb440b60b
evrim <evrim@core.gen.tr>
2012-04-08 21:07:27 +0300

    implement simple-widget in src/coretal/widget.lisp
    Ignore-this: d3176643ad2145eaafe6b890d823d28d
    
    darcs-hash:20120408180727-74ae0-91ffedb9b2cc33cd527e4dc8516d455eb980e35a.gz

--- src/coretal/widget.lisp
+++ src/coretal/widget.lisp
@@ -6,3 +6,12 @@
 (defcomponent simple-widget ()
   ((widget-map :host both :type abstract-widget-map))
   (:ctor make-simple-widget))
+
+(defmethod/remote init ((self simple-widget))
+  (mapcar (lambda (a) (.remove-child self a))
+	  (reverse (slot-value self 'child-nodes))))
+
+(defmethod/remote destroy ((self simple-widget))
+  (mapcar (lambda (a) (.remove-child self a))
+	  (reverse (slot-value self 'child-nodes)))
+  (call-next-method self))
