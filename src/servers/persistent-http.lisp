e0e36035a63ec52fba57a2ff6d5bd1c38aad9edc
evrim <evrim@core.gen.tr>
2012-07-13 00:16:26 +0300

    fix persistent-http-server
    Ignore-this: c22bada4da4e8476f11aa898454ab89
    
    darcs-hash:20120712211626-74ae0-62b7a5df87764f24ce70ffb4a3af7dfb2bd3f2c2.gz

--- src/servers/persistent-http.lisp
+++ src/servers/persistent-http.lisp
@@ -22,18 +22,19 @@
 
 (deftransaction register-to-database ((self persistent-http-server)
 				      (application web-application))
-  (setf (database.get self 'applications)
+  (setf (database.get self :applications)
 	(cons application (unregister-from-database self application))))
 
 (deftransaction unregister-from-database ((self persistent-http-server)
 					  (application web-application)) 
-  (setf (database.get self 'applications)
+  (setf (database.get self :applications)
 	(remove (web-application.fqdn application) 
-		(database.get self 'applications)
+		(database.get self :applications)
 		:test #'equal :key #'web-application.fqdn)))
 
 (defmethod register ((self persistent-http-server)
 		     (application web-application))
+  (describe application)
   (when (web-application.persistent application)
     (register-to-database self application)))
 
@@ -46,13 +47,13 @@
   (mapcar (lambda (application) (register self application))
   	  (filter (lambda (app)
 		    (not (find-application self (web-application.fqdn app))))
-		  (database.get self 'applications))))
+		  (database.get self :applications))))
 
 (defmethod stop ((self persistent-http-server))
   ;; (mapcar (lambda (application) (unregister self application))
   ;; 	  (filter (lambda (app)
   ;; 		    (find-application self (web-application.fqdn app)))
-  ;; 		  (database.get self 'applications)))
+  ;; 		  (database.get self :applications)))
   )
 
 ;; (defclass+ sample-persistent-application (http-application)
