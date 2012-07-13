f6da61ee5f63ed98c108de76149092d8f279f4c6
evrim <evrim@core.gen.tr>
2012-07-13 10:13:27 +0300

    fix <core:tab & <widget:tab
    Ignore-this: 3d46f23d5728522993106453b365b06f
    
    darcs-hash:20120713071327-74ae0-92598ecbdbf83cdfe31f096c6820481c9e30db22.gz

--- src/web/tab.lisp
+++ src/web/tab.lisp
@@ -1,40 +1,56 @@
 (in-package :core-server)
 
 (defcomponent <core:tab (<:div)
-  ((contents-div :host remote)))
+  ((tabs :host remote)
+   (hilight-class :host remote :initform "hilight")
+   (tab-css :host remote :initform +tab-css+)
+   (default-tab :host remote :initform nil)
+   (_content :host remote)
+   (_nav :host remote)))
 
-(defmethod/remote get-titles ((component <core:tab))
-  (list (list "default" "Tab 1")
-	(list "tab2" "Tab 2")))
+(defmethod/remote hilight-tab ((self <core:tab) tab)
+  (mapcar (lambda (li)
+	    (let ((a (car (slot-value li 'child-nodes))))
+	      (cond
+		((eq tab (slot-value a 'inner-text))
+		 (add-class a (hilight-class self))
+		 (add-class li (hilight-class self)))
+		(t
+		 (remove-class a (hilight-class self))
+		 (remove-class li (hilight-class self))))))
+	  (slot-value (_nav self) 'child-nodes)))
 
-(defmethod/remote get-tab ((component <core:tab) tab)
-  (cond
-    ((eq tab "tab2")
-     (<:div :class "tab"
-	    (<:p "Second tab contents")))
-    (t
-     (<:div :class "tab"
-	    (<:p "Default tab contents")))))
+(defmethod/remote make-tab ((self <core:tab) tab)
+  (reduce0 (lambda (acc a)
+	     (if (and (listp a) (eq tab (car a)))
+		 (car (cdr a))
+		 acc))
+	   (tabs self)))
 
-(defmethod/remote show-tab ((component <core:tab) tab)
-  (setf component.contents-div.inner-h-t-m-l "")
-  (.append-child component.contents-div (get-tab component tab)))
+(defmethod/remote show-tab ((self <core:tab) tab)
+  (hilight-tab self tab)
+  (aif (make-tab self tab)
+       (setf (_content self) (replace-node (_content self) it))))
 
-(defmethod/remote get-menu ((component <core:tab))  
-  (<:ul :class "tabs"
-	(mapcar (lambda (title)
-		  (<:li (<:a :onclick (lambda (e)
-					(show-tab component (car title)))
-			     (car (cdr title)))))
-		(get-titles component))))
+(defmethod/remote navigation ((self <core:tab))
+  (<:ul :class "core-tab-navigation block left width-100p"
+   (mapcar-cc (lambda (tab)
+		(<:li (<:a :onclick (lifte (show-tab self tab)) tab)))
+	      (mapcar (lambda (a) (if (atom a) a (car a)))
+		      (tabs self)))))
 
-(defmethod/remote init-tabs ((component <core:tab))
-  (.append-child component (get-menu component))
-  (let ((div (<:div :class "content")))
-    (.append-child component div)
-    (set-contents-div component div))
-  (show-tab component "default")
-  (add-class component "tab"))
+(defmethod/remote destroy ((self <core:tab))
+  (remove-class self "core-tab")
+  (remove-class self "core")
+  (call-next-method self))
 
-(defmethod/remote init ((component <core:tab))
-  (init-tabs component))
+(defmethod/remote init ((self <core:tab))
+  (call-next-method self)
+  (load-css (tab-css self))
+  (add-class self "core-tab")
+  (add-class self "core")
+  (append self (setf (_nav self) (navigation self)))
+  (append self (<:div :class "clear"))
+  (append self (setf (_content self)
+		     (<:p :class "pad10"
+			  "Please select an option from above."))))
