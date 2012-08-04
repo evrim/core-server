6535d0139563c1a47d5ccf3c7ddbd68cf1e01e8c
evrim <evrim@core.gen.tr>
2012-08-04 16:44:45 +0300

    fix <core:simple-content widget
    Ignore-this: e00b8adef3e1fa298fccbbc4ef344f17
    
    darcs-hash:20120804134445-74ae0-ec278b09fbfdc426e2839e4c62dbf97d69eae5af.gz

--- src/widget/content.lisp
+++ src/widget/content.lisp
@@ -3,10 +3,10 @@
 (defcomponent <widget:simple-content (<:div <widget:simple)
   ((content :host remote :initform nil :initarg :children)))
 
-(defmethod/remote destroy ((self <widget:simple-content))
-  (delete-slots self 'content)
-  (call-next-method self))
-
 (defmethod/remote init ((self <widget:simple-content))
   (call-next-method self) 
-  (mapcar (lambda (a) (append self a)) (content self)))
+  (mapcar-cc (lambda (a)
+	       (if (typep a 'function)
+		   (append self (make-component a))
+		   (append self a)))
+	     (content self)))
